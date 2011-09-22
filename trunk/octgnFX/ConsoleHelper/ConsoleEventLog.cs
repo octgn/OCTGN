6b7b3e184a52470ab70902a98b655a2b698fc8af
Kelly Elton <Kelly.Elton@Skylabsonline.com>
2011-09-22 16:01:10 +0000

    Added Skylabs.ConsoleHelper

---
+++ trunk/octgnFX/ConsoleHelper/ConsoleEventLog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Skylabs.ConsoleHelper
+{
+    public class ConsoleEventLog
+    {
+        public delegate void EventEventDelegate(ConsoleEvent e);
+        public static event EventEventDelegate eAddEvent = null;
+
+        public static List<ConsoleEvent> Events { get { return _Events; } set { _Events = value; } }
+
+        private static List<ConsoleEvent> _Events = new List<ConsoleEvent>();
+
+        public static void addEvent(ConsoleEvent con, Boolean writeToConsole)
+        {
+            Events.Add(con);
+            if(writeToConsole)
+                con.writeEvent(false);
+            if(eAddEvent != null)
+            {
+                if(eAddEvent.GetInvocationList().Length != 0)
+                    eAddEvent.Invoke(con);
+            }
+        }
+
+        public static void SerializeEvents(string filename)
+        {
+            try
+            {
+                using(Stream stream = File.Open(filename, FileMode.Create, FileAccess.ReadWrite, FileShare.Write))
+                {
+                    XmlSerializer xs;
+                    XmlTextWriter xmlTextWriter = new XmlTextWriter(stream, Encoding.ASCII);
+                    xmlTextWriter.Formatting = Formatting.Indented;
+                    xmlTextWriter.Indentation = 4;
+                    List<Type> cTypes = new List<Type>();
+                    foreach(ConsoleEvent c in ConsoleEventLog.Events)
+                    {
+                        bool foundit = false;
+                        foreach(Type t in cTypes)
+                        {
+                            if(c.GetType() == t)
+                            {
+                                foundit = true;
+                                break;
+                            }
+                        }
+                        if(!foundit)
+                            cTypes.Add(c.GetType());
+                    }
+                    xs = new XmlSerializer(ConsoleEventLog.Events.GetType(), new XmlAttributeOverrides(), cTypes.ToArray(), new XmlRootAttribute("Events"), "Skylabs.olobby");
+                    xs.Serialize(xmlTextWriter, ConsoleEventLog.Events);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Log error " + e.Message + "\n" + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ trunk/octgnFX/ConsoleHelper/ConsoleReader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+using System.Text;
+using System.Threading;
+
+namespace Skylabs.ConsoleHelper
+{
+    public class ConsoleReader
+    {
+        public delegate void ConsoleInputDelegate(ConsoleMessage input);
+
+        public static event ConsoleInputDelegate eConsoleInput;
+        //public static ConsoleColor InputColor { get { return _InputColor; } set { _InputColor = value; } }
+
+        //private static ConsoleColor _InputColor = ConsoleColor.Gray;
+
+        private static Thread thread;
+        private static Thread rlthread;
+
+        public static ThreadState ThreadState
+        {
+            get
+            {
+                return thread.ThreadState;
+            }
+        }
+
+        public static void Start()
+        {
+            thread = new Thread(run);
+            thread.Name = "ConsoleHandThread";
+            thread.Start();
+        }
+
+        private static void handleInput(ConsoleMessage cm)
+        {
+            if(eConsoleInput != null)
+            {
+                if(eConsoleInput.GetInvocationList().Length > 0)
+                    eConsoleInput.Invoke(cm);
+            }
+        }
+
+        public static void Stop()
+        {
+            if(thread != null)
+            {
+                if(thread.ThreadState == System.Threading.ThreadState.Running)
+                {
+                    Console.In.Close();
+
+                    thread.Abort();
+                    thread = null;
+                    //thread.Join();
+                }
+            }
+        }
+
+        private static void run()
+        {
+            Boolean b = true;
+            Boolean endLine = false;
+            StringBuilder sb = new StringBuilder();
+            while(b == true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo r = Console.ReadKey();
+                    switch(r.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            endLine = true;
+                            break;
+                        default:
+                            sb.Append(r.KeyChar);
+                            break;
+                    }
+                }
+                if(!String.IsNullOrEmpty(sb.ToString()) && endLine)
+                {
+                    handleInput(new ConsoleMessage(sb.ToString()));
+                    sb = new StringBuilder();
+                    endLine = false;
+                }
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
