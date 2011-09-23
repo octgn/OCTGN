6628417aa3aab074da9ac040d626976118cf385f
Kent <mr.magus@gmail.com>
2011-09-23 21:54:19 +0000

    Updated Server Minor Version to allow itself to connect.

--- octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyVersion("0.11.0")]
