fe277f477f208ce0000fbe38291393fcf275c93a
Kelly Elton <Kelly.Elton@Skylabsonline.com>
2011-09-30 07:16:45 +0000

    Finished FriendListItem. User picture now comes from Gravatar.com

--- Controls/FriendListItem.xaml.cs
+++ Controls/FriendListItem.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using Skylabs;
+using Skylabs.Lobby;
 
 namespace Octgn.Controls
 {
@@ -10,38 +12,54 @@ namespace Octgn.Controls
     /// </summary>
     public partial class FriendListItem : UserControl
     {
-        public static DependencyProperty MessageProperty = DependencyProperty.Register(
+        public static DependencyProperty UsernameProperty = DependencyProperty.Register(
     "UserName", typeof(string), typeof(FriendListItem));
+        public static DependencyProperty CustomStatusProperty = DependencyProperty.Register(
+    "CustomStatus", typeof(string), typeof(FriendListItem));
         public static DependencyProperty PictureProperty = DependencyProperty.Register(
     "Picture", typeof(ImageSource), typeof(FriendListItem));
+        public static DependencyProperty StatusPictureProperty = DependencyProperty.Register(
+    "StatusPicture", typeof(ImageSource), typeof(FriendListItem));
 
-        public string UserName
+        private User m_User = new User();
+
+        public User ThisUser
         {
             get
             {
-                return (string)GetValue(MessageProperty);
+                return m_User;
             }
             set
             {
-                SetValue(MessageProperty, value);
-            }
-        }
-
-        public ImageSource Picture
-        {
-            get
-            {
-                return (ImageSource)GetValue(PictureProperty);
-            }
-            set
-            {
-                SetValue(PictureProperty, value);
+                m_User = value;
+                SetValue(CustomStatusProperty, value.CustomStatus);
+                string h = ValueConverters.HashEmailAddress(value.Email.ToLower().Trim());
+                string guri = "http://www.gravatar.com/avatar/" + h + "?s=64&r=x";
+                SetValue(PictureProperty, new ImageSourceConverter().ConvertFromString(guri) as ImageSource);
+                SetValue(UsernameProperty, value.DisplayName);
+                switch(value.Status)
+                {
+                    case UserStatus.Away:
+                        guri = @"pack://application:,,,/OCTGN;component/Resources/statusAway.png";
+                        break;
+                    case UserStatus.DoNotDisturb:
+                        guri = @"pack://application:,,,/OCTGN;component/Resources/statusDND.png";
+                        break;
+                    case UserStatus.Online:
+                        guri = @"pack://application:,,,/OCTGN;component/Resources/statusOnline.png";
+                        break;
+                    default: //Offline or anything else
+                        guri = @"pack://application:,,,/OCTGN;component/Resources/statusOffline.png";
+                        break;
+                }
+                SetValue(StatusPictureProperty, new ImageSourceConverter().ConvertFromString(guri) as ImageSource);
             }
         }
 
         public FriendListItem()
         {
             InitializeComponent();
+            ThisUser = new User();
         }
 
         private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
