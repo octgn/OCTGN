67aa90d3c60a6f7af1e05678cf67d522b06fe457
Kelly Elton <Kelly.Elton@Skylabsonline.com>
2011-10-07 19:13:53 +0000

    Added NotificationListItem for Notification page.

---
+++ octgnFX/Octgn/Controls/NotificationListItem.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Octgn.Controls
+{
+    /// <summary>
+    /// Interaction logic for NotificationListItem.xaml
+    /// </summary>
+    public partial class NotificationListItem : UserControl
+    {
+        public event MouseEventHandler OnClose;
+        public int NotificationNumber = -1;
+        public static DependencyProperty UsernameProperty = DependencyProperty.Register(
+    "Message", typeof(string), typeof(FriendListItem));
+        public static DependencyProperty PictureProperty = DependencyProperty.Register(
+    "Picture", typeof(ImageSource), typeof(FriendListItem));
+
+        public NotificationListItem()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Focus();
+        }
+
+        private void imgDismiss_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(OnClose != null)
+                OnClose.Invoke(this, new MouseEventArgs(e.MouseDevice, e.Timestamp));
+        }
+    }
+}
