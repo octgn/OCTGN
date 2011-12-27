53688beaca06ec482b3cacf6b583d500e399d82e
Kent <mr.magus@gmail.com>
2011-12-27 18:22:45 +0000

    added "Check for updates" menu entry.

--- Launcher/Login.xaml.cs
+++ Launcher/Login.xaml.cs
@@ -170,6 +170,79 @@ namespace Octgn.Launcher
             Program.Exit();
         }
 
+        private bool FileExists(string URL)
+        {
+            bool result = false;
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                try
+                {
+                    System.IO.Stream str = client.OpenRead(URL);
+                    if (str != null) result = true; else result = false;
+                }
+                catch
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        private string[] ReadUpdateXML(string URL)
+        {
+            string[] values = new string[2];
+
+            using (System.Xml.XmlReader reader = System.Xml.XmlReader.Create(System.Net.WebRequest.Create(URL).GetResponse().GetResponseStream()))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (!reader.IsEmptyElement)
+                        {
+                            switch (reader.Name)
+                            {
+                                case "Version":
+                                    values = new string[2];
+                                    if (reader.Read()) { values[0] = reader.Value; }
+                                    break;
+                                case "Location":
+                                    if (reader.Read()) { values[1] = reader.Value; }                                    
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            return values;
+        }
+
+        private void menuUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileExists("http://www.skylabsonline.com/octgn_update/update.xml"))
+            {
+                string[] update = new string[2];
+                update = ReadUpdateXML("http://www.skylabsonline.com/octgn_update/update.xml");
+
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                Version local = assembly.GetName().Version;
+                Version online = new Version(update[0]);
+                if (online > local)
+                {
+                    switch (MessageBox.Show("An update is available. Would you like to download now?", "Update Available", MessageBoxButton.YesNo, MessageBoxImage.Question))
+                    {
+                        case MessageBoxResult.Yes:
+                            System.Diagnostics.Process.Start(update[1]);
+                            Program.Exit();
+                            break;
+                        case MessageBoxResult.No:
+                            //
+                            break;
+                    }
+                } 
+            }
+        }
+
         private void menuOctgn_Click(object sender, RoutedEventArgs e)
         {
         }
