0f4e76894afdc45c6917b2189d051a518218789a
Kelly Elton <Kelly.Elton@Skylabsonline.com>
2012-01-11 18:22:23 +0000

    Added some more webserver stuff Made a locking change in LobbyServer.Server

--- trunk/octgnFX/Octgn.LobbyServer/WebServer.cs
+++ trunk/octgnFX/Octgn.LobbyServer/WebServer.cs
@@ -19,7 +19,16 @@ namespace Skylabs.LobbyServer
             _running = false;
             _thread =new Thread(Run);
             _server = new HttpListener();
-            _server.Prefixes.Add("http://+:8901/");
+            int port = 8901;
+            try
+            {
+                port = Int32.Parse(Program.Settings["webserverport"]);
+            }
+            catch (Exception)
+            {
+                port = 8901;
+            }
+            _server.Prefixes.Add(String.Format("http://+:{0}/",port));
         }
         public bool Start()
         {
@@ -52,23 +61,50 @@ namespace Skylabs.LobbyServer
 
                 var page = req.Url.AbsolutePath.Trim('/');
                 page = page.ToLower();
-                switch(page)
+                switch (page)
                 {
                     case "":
                         {
-                            Version v = Assembly.GetCallingAssembly().GetName().Version;
                             var spage = File.ReadAllText("webserver/index.htm");
-                            spage = spage.Replace("$version", v.ToString());
-                            byte[] buffer = Encoding.UTF8.GetBytes(spage);
-                            con.Response.ContentLength64 = buffer.Length;
-                            using (Stream o = con.Response.OutputStream)
+                            spage = ReplaceVariables(spage);
+                            SendItem(con.Response, spage);
+                            break;
+                        }
+                    default:
+                        {
+                            var spage = "";
+                            try
                             {
-                                o.Write(buffer, 0, buffer.Length);
-                                o.Close();
+                                spage = File.ReadAllText("webserver/" + page);
                             }
+                            catch (Exception)
+                            {
+                                spage = "";
+                                con.Response.StatusCode = 404;
+                            }
+                            spage = ReplaceVariables(spage);
+                            SendItem(con.Response, spage);
                             break;
                         }
                 }
+            }
+        }
+        private string ReplaceVariables(string rawpage)
+        {
+            string ret = rawpage;
+            Version v = Assembly.GetCallingAssembly().GetName().Version;
+            ret = rawpage.Replace("$version", v.ToString());
+            ret = ret.Replace("$runtime", Server.ServerRunTime.ToString());
+            return ret;
+        }
+        private void SendItem(HttpListenerResponse res,string page)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(page);
+            res.ContentLength64 = buffer.Length;
+            using (Stream o = res.OutputStream)
+            {
+                o.Write(buffer, 0, buffer.Length);
+                o.Close();
             }
         }
     }
