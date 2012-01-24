a6ccb6b53fa85a633e10f27745f6bbecabce5048
Kelly Elton <Kelly.elton@Skylabsonline.com>
2012-01-23 20:48:01 -0600

    Completed removing all licensing info.

--- octgnFX/Skylabs.Lobby/Sockets/SocketMessage.cs
+++ octgnFX/Skylabs.Lobby/Sockets/SocketMessage.cs
@@ -1,8 +1,4 @@
-﻿//Copyright 2012 Skylabs
-//In order to use this software, in any manor, you must first contact Skylabs.
-//Website: http://www.skylabsonline.com
-//Email:   skylabsonline@gmail.com
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
