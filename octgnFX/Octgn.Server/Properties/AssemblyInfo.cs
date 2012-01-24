2d8b763ac3d7083773afa1f2db181ace5f5db597
Kelly Elton <Kelly.elton@Skylabsonline.com>
2012-01-23 21:19:17 -0600

    Lobby+Server+StandAloneServer to v3.0.0.6
    Octgn to 3.0.0.4

--- octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.4")]
+[assembly: AssemblyFileVersionAttribute("3.0.0.4")]
