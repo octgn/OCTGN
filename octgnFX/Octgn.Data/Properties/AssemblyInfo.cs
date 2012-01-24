5d3d6871ba987ef7bd9332b34eaa20717ed01156
Kelly Elton <Kelly.elton@Skylabsonline.com>
2012-01-24 03:22:33 -0600

    octgn 3.0.0.5
    lobby 3.0.0.7

--- octgnFX/Octgn.Data/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Data/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.4")]
-[assembly: AssemblyFileVersionAttribute("3.0.0.4")]
+[assembly: AssemblyVersion("3.0.0.5")]
+[assembly: AssemblyFileVersionAttribute("3.0.0.5")]
