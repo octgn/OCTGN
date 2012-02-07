45e2e836b57cb45a31d7258784511fe13d7aa8f0
Robert Melton <robert@robertmelton.com>
2012-02-07 14:18:40 -0500

    Fixed issues in MultiLogin

--- octgnFX/Skylabs.MultiLogin/Class1.cs
+++ octgnFX/Skylabs.MultiLogin/Class1.cs
@@ -2,11 +2,6 @@
 //In order to use this software, in any manor, you must first contact Skylabs.
 //Website: http://www.skylabsonline.com
 //Email:   skylabsonline@gmail.com
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Skylabs.MultiLogin
 {
     public abstract class ILogin
