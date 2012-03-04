1f878a5fc6402ddd75326e6d7390d462515a72cc
Magnus Vortex <magnusvortex@gmail.com>
2012-03-04 10:43:59 -0600

    Edited comment to be more precise

--- octgnFX/Octgn/DeckBuilder/SearchControl.xaml.cs
+++ octgnFX/Octgn/DeckBuilder/SearchControl.xaml.cs
@@ -139,7 +139,7 @@ namespace Octgn.DeckBuilder
             }
             //SearchString.Text = ConvertToSQLString(conditions);
             //TODO Implement a way to take the text of SearchString and use it as the search parameters. 
-            //It should be exactly the SQL Query, so no parsing *should* be needed (but be prepared for it to fail).
+            //It *should* be exactly the SQL Query, but it's input by the user (read: prone to typos)
             resultsGrid.ItemsSource = Game.SelectCards(conditions).DefaultView;
         }
         private string ConvertToSQLString(string[] conditions)
