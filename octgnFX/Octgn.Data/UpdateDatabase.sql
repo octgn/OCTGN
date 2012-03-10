d3199e4d91c07f0130e3df32895aa1c69ee66736
kellyelton <kelly.elton@skylabsonline.com>
2012-03-09 23:30:49 -0600

    Updated database updater to version 3. Added columns dependant and mutable to cards table.

--- octgnFX/Octgn.Data/UpdateDatabase.sql
+++ octgnFX/Octgn.Data/UpdateDatabase.sql
@@ -1,7 +1,14 @@
 ﻿begin transaction;
 SELECT [version] as ver FROM [dbinfo];
-CASE ver WHEN ver = 1 THEN
+WHEN ver = 1 THEN
     UPDATE [dbinfo] SET [version]=2;
+    ver = 2;
     ALTER TABLE [cards] ADD COLUMN [alternate] TEXT NOT NULL;
+END
+WHEN ver = 2 THEN
+    UPDATE [dbinfo] SET [version]=3;
+    ver = 3;
+    ALTER TABLE [cards] ADD COLUMN [dependent] BOOLEAN;
+    ALTER TABLE [cards] ADD COLUMN [mutable] BOOLEAN;
 END
 commit transaction;
