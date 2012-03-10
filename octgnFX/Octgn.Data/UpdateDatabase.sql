d3908100ae6cf55d494254f521a64007b1d14cf9
kellyelton <kelly.elton@skylabsonline.com>
2012-03-09 23:19:43 -0600

    Added update to version 2 code. Added alternate column to cards table.

--- octgnFX/Octgn.Data/UpdateDatabase.sql
+++ octgnFX/Octgn.Data/UpdateDatabase.sql
@@ -1,1 +1,7 @@
-﻿SELECT * FROM dbinfo;
+﻿begin transaction;
+SELECT [version] as ver FROM [dbinfo];
+CASE ver WHEN ver = 1 THEN
+    UPDATE [dbinfo] SET [version]=2;
+    ALTER TABLE [cards] ADD COLUMN [alternate] TEXT NOT NULL;
+END
+commit transaction;
