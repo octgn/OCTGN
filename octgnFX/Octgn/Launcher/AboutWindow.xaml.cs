72e58923a8d884a99cde21093ea0987b0972558c
drilus <mr.magus@gmail.com>
2012-04-04 16:22:34 -0400

    adding a few more finishing touches to the About page

--- octgnFX/Octgn/Launcher/AboutWindow.xaml.cs
+++ octgnFX/Octgn/Launcher/AboutWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Octgn.Launcher
     {
         public AboutWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -77,6 +77,75 @@ namespace Octgn.Launcher
             textRalig98.BeginAnimation(OpacityProperty, hide);
         }
 
-        #endregion
+        private void imgGravecorp_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, hide);
+            textGravecorp.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void imgGravecorp_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(0.25, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation show = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, show);
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textGravecorp.BeginAnimation(OpacityProperty, hide);
+        }
+
+        private void imgD0c_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, hide);
+            textD0c.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void imgD0c_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(0.25, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation show = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, show);
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textD0c.BeginAnimation(OpacityProperty, hide);
+        }
+
+        private void imgOther_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, hide);
+            textOther.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void imgOther_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+            DoubleAnimation animation = new DoubleAnimation(0.25, TimeSpan.FromSeconds(0.5));
+            image.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation show = new DoubleAnimation(1, TimeSpan.FromSeconds(0.5));
+            textAbout.BeginAnimation(OpacityProperty, show);
+            DoubleAnimation hide = new DoubleAnimation(0, TimeSpan.FromSeconds(0.5));
+            textOther.BeginAnimation(OpacityProperty, hide);
+        }
+
+        #endregion                                
     }
 }
