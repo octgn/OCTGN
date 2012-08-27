ccc8643bd7c7b4f614e83add9ef02ca47d1e6eed
Kelly Elton <Kelly.elton@Skylabsonline.com>
2012-08-26 20:25:08 -0500

    Added NUnit Added version number test. Test fails on purpose to see if remote build fails.

---
+++ octgnFX/Octgn.Test/Octgn.Test/VersionTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace Octgn.Test
+{
+    public class VersionTest
+    {
+        [Test]
+        public void CheckAssemblyVersions()
+        {
+            //Get the correct version number from currentversion.xml
+            var vstream = Assembly.GetAssembly(typeof (Program)).GetManifestResourceStream("Octgn.CurrentVersion.txt");
+            var versionString = "";
+            using(var sr = new StreamReader(vstream))
+            {
+                versionString = sr.ReadToEnd().Trim();
+            }
+            Debug.WriteLine("Specified Version: {0}",(object)versionString);
+            Version realVersion = null;
+            Assert.True(Version.TryParse(versionString,out realVersion));
+
+            Debug.WriteLine("Check Version Of Octgn");
+            Assert.AreEqual(realVersion , typeof (Program).Assembly.GetName().Version);
+            Debug.WriteLine("Check Version Of Octgn.Data");
+            Assert.AreEqual(realVersion , typeof(Octgn.Data.Game).Assembly.GetName().Version);
+            Debug.WriteLine("Check Version Of Octgn.Server");
+            Assert.AreEqual(realVersion  ,typeof(Octgn.Server.Server).Assembly.GetName().Version);
+            Debug.WriteLine("Check Version Of Octgn.StandAloneServer");
+            Assert.AreEqual(realVersion , typeof(Octgn.StandAloneServer.Program).Assembly.GetName().Version);
+            Debug.WriteLine("Check Version Of Skylabs.Lobby");
+            Assert.AreEqual(realVersion , typeof(Skylabs.Lobby.Client).Assembly.GetName().Version);
+            Debug.WriteLine("Check Version Of Octgn.LobbyServer");
+            Assert.AreEqual(realVersion , typeof(Skylabs.LobbyServer.Program).Assembly.GetName().Version);
+            Assert.True(false);
+        }
+    }
+}
