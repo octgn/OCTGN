22e743725025eb40d277cb85ac4011dfc20f622c
Kelly Elton <Kelly.elton@Skylabsonline.com>
2012-08-31 23:58:25 -0500

    Backed version number to 3.0.1.14

--- octgnFX/Octgn.ReleasePusher/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.ReleasePusher/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Octgn.ReleasePusher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OCTGN")]
 [assembly: AssemblyProduct("Octgn.ReleasePusher")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  OCTGN 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.14")]
+[assembly: AssemblyFileVersion("3.0.1.14")]
