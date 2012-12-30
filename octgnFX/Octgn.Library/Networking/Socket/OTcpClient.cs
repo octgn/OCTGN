bee9643baeb512d70018538f838435a7a7ffc60a
Kelly Elton <kelly.elton@skylabsonline.com>
2012-12-29 21:22:03 -0600

    Added Octgn.Library project Started work on a client server system Added log4net for logging.

---
+++ octgnFX/Octgn.Library/Networking/Socket/OTcpClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octgn.Library.Networking.Socket
+{
+    using System.Net;
+    using System.Net.Sockets;
+
+    using log4net;
+
+    public class OTcpClient
+    {
+        public event Action<EventArgs,OTcpClient> ClientClosed;
+
+        private readonly ILog log = LogManager.GetLogger(typeof(OTcpClient));
+
+        public EndPoint EndPoint { get
+        {
+            return Client.Client.RemoteEndPoint;
+        } }
+
+        protected TcpClient Client { get; private set; }
+
+        public OTcpClient(TcpClient client)
+        {
+            this.log.InfoFormat("Created from TcpClient {0}",client.Client.RemoteEndPoint);
+            Client = client;
+        }
+
+        #region Event Invocators
+        protected void OnClientClosed(OTcpClient client)
+        {
+            var handler = this.ClientClosed;
+            if (handler != null)
+            {
+                handler(EventArgs.Empty, client);
+            }
+        }
+        #endregion
+    }
+}
---
+++ octgnFX/Octgn.Library/Networking/Socket/OTcpListener.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octgn.Library.Networking.Socket
+{
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Reflection;
+    using System.Threading;
+
+    using log4net;
+
+    public class OTcpListener
+    {
+        protected TcpListener Listener { get; set; }
+        protected Int32 ClientCount;
+
+        private readonly ILog log = LogManager.GetLogger(typeof(OTcpListener));
+
+        public OTcpListener( IPEndPoint bind )
+        {
+            if(bind == null)
+                throw new ArgumentNullException("bind cannot be null.");
+            
+            Listener = new TcpListener(bind);
+        }
+
+        public void Start()
+        {
+            this.log.InfoFormat("Starting on {0}",((IPEndPoint)Listener.LocalEndpoint).ToString());
+            Listener.Start(5);
+            Listener.BeginAcceptTcpClient(this.AcceptClientCallback, Listener);
+            this.log.InfoFormat("OTcpListener started on {0}",this.Listener.LocalEndpoint.ToString());
+        }
+
+        #region Callbacks
+
+        private void AcceptClientCallback(IAsyncResult res)
+        {
+            try
+            {
+                var c = new OTcpClient(Listener.EndAcceptTcpClient(res));
+                c.ClientClosed += OnClientClosed;
+                Interlocked.Increment(ref ClientCount);
+                this.log.InfoFormat("Client connected on {0}",c.EndPoint);
+                this.log.InfoFormat("Client Count {0}", ClientCount);
+            }
+            catch (Exception e)
+            {
+                this.log.Error("AccpetClientCallback",e);
+            }
+        }
+
+        private void OnClientClosed(EventArgs eventArgs, OTcpClient oTcpClient)
+        {
+            Interlocked.Decrement(ref ClientCount);
+            this.log.InfoFormat("Client Count {0}", ClientCount);
+        }
+
+        #endregion
+
+    }
+}
