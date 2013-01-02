b25af3380d393fcf9abb0069b5a44e771d6dc86f
Kelly Elton <kelly.elton@skylabsonline.com>
2013-01-01 18:35:26 -0600

    Removed RavenDB

--- octgnFX/Octgn.Data/GameRepository.cs
+++ octgnFX/Octgn.Data/GameRepository.cs
@@ -3,10 +3,6 @@
     using System;
     using System.IO.Abstractions;
     using System.Linq;
-    using System.Reflection;
-
-    using Raven.Client;
-    using Raven.Client.Embedded;
 
     /// <summary>
     /// Provides access to all the game, set, cards etc.
@@ -14,11 +10,6 @@
     public class GameRepository : IGameRepository
     {
         #region Static
-        /// <summary>
-        /// Database
-        /// </summary>
-        private static EmbeddableDocumentStore database;
-
         /// <summary>
         /// Get an instance of IGameRepository
         /// </summary>
@@ -35,35 +26,37 @@
         {
             get
             {
-                return this.GetSession().Query<Game>();
+                return null;
             }
         } 
 
         public GameRepository()
         {
-            if(FS == null)
-                FS = new FileSystem();
-            if (database != null)
-                return;
-            var dbConfigType =
-                (from t in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x=>x.GetTypes())
-                 where t.GetInterfaces().Any(x=>x == typeof(IDatabaseConfig))
-                 select t).FirstOrDefault();
-            if(dbConfigType == null)
-                throw new Exception("Database config class missing.");
+            //if(FS == null)
+            //    FS = new FileSystem();
+            //if (database != null)
+            //    return;
+            //var dbConfigType =
+            //    (from t in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x=>x.GetTypes())
+            //     where t.GetInterfaces().Any(x=>x == typeof(IDatabaseConfig))
+            //     select t).FirstOrDefault();
+            //if(dbConfigType == null)
+            //    throw new Exception("Database config class missing.");
 
-            var dbConfig = (IDatabaseConfig)Activator.CreateInstance(dbConfigType);
+            //var dbConfig = (IDatabaseConfig)Activator.CreateInstance(dbConfigType);
 
-            database = new EmbeddableDocumentStore() { DataDirectory = dbConfig.DataPath, UseEmbeddedHttpServer = false };
-            if (!FS.Directory.Exists(dbConfig.DataPath)) FS.Directory.CreateDirectory(dbConfig.DataPath);
-            database.Initialize();
+            //database = new EmbeddableDocumentStore()
+            //               {
+            //                   DataDirectory = dbConfig.DataPath, 
+            //                   UseEmbeddedHttpServer = true
+            //               };
+            //NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(database.Configuration.Port);
+            //if (!FS.Directory.Exists(dbConfig.DataPath)) FS.Directory.CreateDirectory(dbConfig.DataPath);
+            //database.Initialize();
+
+            //DocumentStore s = new DocumentStore() { Url = "http://localhost:" + database.Configuration.Port.ToString() };
+            //s.Initialize();
+
         }
-
-        private IDocumentSession GetSession()
-        {
-            return database.OpenSession();
-        }
-
-
     }
 }
