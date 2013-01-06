305b57e00fca9785b71da9716df0d74ea2e24646
Kelly Elton <kelly.elton@skylabsonline.com>
2013-01-05 19:55:32 -0600

    Removed Fluent Nhibernate Added some tests to DeckBuilderPluginExample Added nuget package for Octgn for Sqlite

--- octgnFX/Octgn.Data/Database/Entities/DbInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Octgn.Data.Database.Entities
-{
-    public class DbInfo
-    {
-        public virtual int Version { get; set; }
-    }
-}
--- octgnFX/Octgn.Data/Database/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Octgn.Data.Database.Entities
-{
-    public class User
-    {
-        public virtual int Id { get; set; }
-        public virtual string Jid { get; set; }
-        public virtual string Email { get; set; }
-    }
-}
