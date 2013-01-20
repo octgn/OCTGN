5d161083d9ae0ebd2c9e2078a6ae9986885be018
Kelly Elton <kelly.elton@skylabsonline.com>
2013-01-19 18:32:24 -0600

    Speed fix for lobby chat.

---
+++ octgnFX/Octgn.Library/Utils/SortableObservableCollection.cs
@@ -0,0 +1,29 @@
+﻿namespace Octgn.Library.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    public class SortableObservableCollection<T> : ObservableCollection<T>
+    {
+        private bool isSorting = false;
+
+        public void Sort()
+        {
+            Sort(Comparer<T>.Default);
+        }
+
+        public void Sort(IComparer<T> comparer)
+        {
+            isSorting = true;
+            var arr = this.Items.ToArray();
+            Array.Sort(arr,comparer);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                this[i] = arr[i];
+            }
+            isSorting = false;
+        }
+    }
+}
