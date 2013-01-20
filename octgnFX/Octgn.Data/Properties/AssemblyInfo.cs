be1ee020fbd6e33c1af903b70124bfe4cba45d2d
Jenkins CI <octgngame@gmail.com>
2013-01-20 00:39:27 +0000

    Auto generated commit for version 3.0.1.30

--- octgnFX/Octgn.Data/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Data/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.1.29")]
-[assembly: AssemblyFileVersion("3.0.1.29")]
+[assembly: AssemblyVersion("3.0.1.30")]
+[assembly: AssemblyFileVersion("3.0.1.30")]
--- octgnFX/Octgn.DeckBuilderPluginExample/DeckBuilderPluginExample.cs
+++ octgnFX/Octgn.DeckBuilderPluginExample/DeckBuilderPluginExample.cs
@@ -59,8 +59,8 @@ namespace Octgn.DeckBuilderPluginExample
         {
             get
             {
-                // Don't allow this plugin to be used in any version less than 3.0.1.29
-                return Version.Parse("3.0.1.29");
+                // Don't allow this plugin to be used in any version less than 3.0.1.30
+                return Version.Parse("3.0.1.30");
             }
         }
     }
