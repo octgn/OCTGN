05dad1a8c5e1ca0c21b9130bbee777fb0bce682c
Jenkins CI <octgngame@gmail.com>
2013-01-25 04:40:17 +0000

    Auto generated commit for version 3.0.1.35

--- octgnFX/Octgn.StandAloneServer/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.StandAloneServer/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.1.34")]
-[assembly: AssemblyFileVersion("3.0.1.34")]
+[assembly: AssemblyVersion("3.0.1.35")]
+[assembly: AssemblyFileVersion("3.0.1.35")]
