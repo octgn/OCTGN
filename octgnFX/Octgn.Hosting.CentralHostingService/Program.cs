666f8633fce983bc0bf3dc02e8d40d1e85ddf449
Kelly Elton <kelly.elton@skylabsonline.com>
2013-02-03 14:53:19 -0600

    Start for hosting services.

---
+++ octgnFX/Octgn.Hosting.CentralHostingService/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Octgn.Hosting.Services
+{
+    using System.ServiceProcess;
+
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new CentralHostingService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ octgnFX/Octgn.Hosting.CentralHostingService/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Octgn.Hosting.Services")]
+[assembly: AssemblyDescription("Octgn 3.0 Hosting Services")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Octgn")]
+[assembly: AssemblyProduct("Octgn.Hosting.Services")]
+[assembly: AssemblyCopyright("Copyright ©  Octgn 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e097325f-d8b0-4314-ab17-694afb00e1de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.4.46")]
+[assembly: AssemblyFileVersion("3.0.4.46")]
