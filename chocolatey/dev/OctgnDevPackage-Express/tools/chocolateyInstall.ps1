51d43fbf983f22eddcd3f853e5acc5cfd8537179
KellyElton <kelly.elton@skylabsonline.com>
2013-02-11 16:32:27 -0600

    Fixed chocolatey package

--- chocolatey/dev/OctgnDevPackage-Express/tools/chocolateyInstall.ps1
+++ chocolatey/dev/OctgnDevPackage-Express/tools/chocolateyInstall.ps1
@@ -6,4 +6,4 @@ cinst msysgit
 cinst TortoiseGit
 
 #Install Ribbon Control
-Install-ChocolateyPackage 'MsRibbonControl' 'msi' '/quite' 'http://download.microsoft.com/download/F/5/5/F550BB8A-8ADC-4DEA-9257-21C3AC43DD5E/Microsoft%20Ribbon%20for%20WPF.msi' 
+Install-ChocolateyPackage 'MsRibbonControl' 'msi' '/quiet' 'http://download.microsoft.com/download/F/5/5/F550BB8A-8ADC-4DEA-9257-21C3AC43DD5E/Microsoft%20Ribbon%20for%20WPF.msi' 
