2dcb96086f009061867452f500de660c968299b4
Travis Bronson <tbronso@gmail.com>
2013-02-12 00:15:25 -0500

    add some comments to make SortShuffle hopefully easier to follow

--- octgnFX/Octgn/Play/Pile.cs
+++ octgnFX/Octgn/Play/Pile.cs
@@ -138,32 +138,42 @@ namespace Octgn.Play
         }
 
         // Quick sort pile to shuffle. Return false if 2 cards were assigned equal values.
+        // Standard quick sort algo. Repeatedly repartition into lower and larger partitions
+        // around a pivot point that starts in the middle.
         internal bool SortShuffle(uint[] a, int left, int right, CardIdentity[] cis, bool local)
         {
             int i = left;
             int j = right;
-            uint x = a[(left + right) / 2];
+            // pivot point that forms the 2 partitions
+            uint pivot = a[(left + right) / 2];
             uint w = 0;
             while (i <= j)
             {
-                while (a[i] < x)
+                // find first entry in left partition not smaller than pivot
+                while (a[i] < pivot)
                 {
                     i++;
                 }
-                while (x < a[j])
+                // find last entry in right partition not larger than pivot
+                while (pivot < a[j])
                 {
                     j--;
                 }
+                // was there a larger entry left of pivot or smaller entry right of pivot?
                 if (i <= j)
                 {
+                    // if 2 cards were assigned the same value (extremely rare chance)
+                    // we will abort and start over to ensure a proper unbiased shuffle.
                     if (a[i] == w || a[j] == w)
                         return false;
+                    // for DoShuffle()
                     if (cis != null)
                     {
                         CardIdentity ci = cis[i];
                         cis[i] = cis[j];
                         cis[j] = ci;
                     }
+                    // for ShuffleAlone()
                     if (local)
                     {
                         Card temp = cards[i];
@@ -171,16 +181,19 @@ namespace Octgn.Play
                         cards[j] = temp;
                     }
 
+                    // swap them around the pivot
                     w = a[i];
                     a[i++] = a[j];
                     a[j--] = w;
                 }
             }
+            // continue sort on the left partition
             if (left < j)
             {
                 if (!SortShuffle(a, left, j, cis, local))
                     return false;
             }
+            // continue sort on the right partition
             if (i < right)
             {
                 if (!SortShuffle(a, i, right, cis, local))
