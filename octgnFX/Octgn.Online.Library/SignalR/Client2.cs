5fc747f8169d04019ba5246b127a66e258566446
Kelly Elton <kelly.elton@skylabsonline.com>
2013-02-17 23:46:41 -0600

    Working on new client structure.

---
+++ octgnFX/Octgn.Online.Library/SignalR/Client2.cs
@@ -0,0 +1,43 @@
+﻿namespace Octgn.Online.Library.SignalR
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNet.SignalR.Client.Hubs;
+
+    public class Client2 : HubConnection
+    {
+        public Client2(string url)
+            : base(url)
+        {
+        }
+
+        public Client2(string url, bool useDefaultUrl)
+            : base(url, useDefaultUrl)
+        {
+        }
+
+        public Client2(string url, string queryString)
+            : base(url, queryString)
+        {
+        }
+
+        public Client2(string url, string queryString, bool useDefaultUrl)
+            : base(url, queryString, useDefaultUrl)
+        {
+        }
+
+        public Client2(string url, IDictionary<string, string> queryString)
+            : base(url, queryString)
+        {
+        }
+
+        public Client2(string url, IDictionary<string, string> queryString, bool useDefaultUrl)
+            : base(url, queryString, useDefaultUrl)
+        {
+        }
+    }
+    public static class Client2ExtensionMethods
+    {
+        
+    }
+}
