7f89b4731f7c571f4ec8a7ebc5e8adc558ee8153
Kelly Elton <kelly.elton@skylabsonline.com>
2013-02-19 22:14:43 -0600

    Hosted game list return

--- octgnFX/Octgn.Online.Library/SignalR/Coms/IGameServiceToSASManagerService.cs
+++ octgnFX/Octgn.Online.Library/SignalR/Coms/IGameServiceToSASManagerService.cs
@@ -1,11 +1,16 @@
 ﻿namespace Octgn.Online.Library.SignalR.Coms
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+
+    using Octgn.Online.Library.SignalR.Coms.Models;
 
     public interface IGameServiceToSASManagerService
     {
         Task StartGame(Guid id, string name, string oGameName, Guid oGameId);
+
+        Task<IEnumerable<HostedGame>> GameList();
 
         Task Hello(string mess1, string mess2);
     }
--- octgnFX/Octgn.Online.SASManager/Coms/GameServiceToSASManagerService.cs
+++ octgnFX/Octgn.Online.SASManager/Coms/GameServiceToSASManagerService.cs
@@ -1,15 +1,24 @@
 ﻿namespace Octgn.Online.SASManagerService.Coms
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using Octgn.Online.Library.SignalR.Coms;
+    using Octgn.Online.Library.SignalR.Coms.Models;
 
     public class GameServiceToSASManagerService : IGameServiceToSASManagerService
     {
         public Task StartGame(Guid id, string name, string oGameName, Guid oGameId)
         {
             return new Task(() => { });
+        }
+
+        public Task<IEnumerable<HostedGame>> GameList()
+        {
+            var ret = new Task<IEnumerable<HostedGame>>(() => { return new HostedGame[0];});
+            ret.Start();
+            return ret;
         }
 
         public Task Hello(string mess1, string mess2)
