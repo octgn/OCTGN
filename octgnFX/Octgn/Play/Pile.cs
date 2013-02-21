d05e84ed39df84928b3e046bab4bd1aa33df309c
Travis Bronson <tbronso@gmail.com>
2013-02-20 22:42:45 -0500

    revert to original implementation

--- octgnFX/Octgn/Play/Pile.cs
+++ octgnFX/Octgn/Play/Pile.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Collections.Generic;
 using Octgn.Definitions;
 using Octgn.Utils;
 
@@ -100,7 +99,6 @@ namespace Octgn.Play
                 }
             }
             // Shuffle
-            bool uniqueVals = true;
             do
             {
                 for (int i = 0; i < cards.Count; i++)
@@ -108,16 +106,7 @@ namespace Octgn.Play
                     rnd.GetBytes(rndbytes);
                     cardRnds[i] = BitConverter.ToUInt32(rndbytes, 0);
                 }
-                Array.Sort(cardRnds, cis);
-                for (int i = 1; i < cards.Count; i++)
-                {
-                    if (cardRnds[i] == cardRnds[i - 1])
-                    {
-                        uniqueVals = false;
-                        break;
-                    }
-                }
-            } while (!uniqueVals);
+            } while (!SortShuffle(cardRnds, 0, cards.Count - 1, cis, false));
 
             // Shuffle complete, build arrays
             for (int i = 0; i < cards.Count; i++)
@@ -137,7 +126,6 @@ namespace Octgn.Play
         {
             var rndbytes = new Byte[4];
             var cardRnds = new uint[cards.Count];
-            bool uniqueVals = true;
             do
             {
                 for (int i = 0; i < cards.Count; i++)
@@ -145,17 +133,73 @@ namespace Octgn.Play
                     rnd.GetBytes(rndbytes);
                     cardRnds[i] = BitConverter.ToUInt32(rndbytes, 0);
                 }
-                Array.Sort(cardRnds, cards.ToArray());
-                for (int i = 1; i < cards.Count; i++)
-                {
-                    if (cardRnds[i] == cardRnds[i - 1])
-                    {
-                        uniqueVals = false;
-                        break;
-                    }
-                }
-            } while (!uniqueVals);
+            } while (!SortShuffle(cardRnds, 0, cards.Count - 1, null, true));
             OnShuffled();
+        }
+
+        // Quick sort pile to shuffle. Return false if 2 cards were assigned equal values.
+        // Standard quick sort algo. Repeatedly repartition into lower and larger partitions
+        // around a pivot point that starts in the middle.
+        internal bool SortShuffle(uint[] a, int left, int right, CardIdentity[] cis, bool local)
+        {
+            int i = left;
+            int j = right;
+            // pivot point that forms the 2 partitions
+            uint pivot = a[(left + right) / 2];
+            uint w = 0;
+            while (i <= j)
+            {
+                // find first entry in left partition not smaller than pivot
+                while (a[i] < pivot)
+                {
+                    i++;
+                }
+                // find last entry in right partition not larger than pivot
+                while (pivot < a[j])
+                {
+                    j--;
+                }
+                // was there a larger entry left of pivot or smaller entry right of pivot?
+                if (i <= j)
+                {
+                    // if 2 cards were assigned the same value (extremely rare chance)
+                    // we will abort and start over to ensure a proper unbiased shuffle.
+                    if (a[i] == w || a[j] == w)
+                        return false;
+                    // for DoShuffle()
+                    if (cis != null)
+                    {
+                        CardIdentity ci = cis[i];
+                        cis[i] = cis[j];
+                        cis[j] = ci;
+                    }
+                    // for ShuffleAlone()
+                    if (local)
+                    {
+                        Card temp = cards[i];
+                        cards[i] = cards[j];
+                        cards[j] = temp;
+                    }
+
+                    // swap them around the pivot
+                    w = a[i];
+                    a[i++] = a[j];
+                    a[j--] = w;
+                }
+            }
+            // continue sort on the left partition
+            if (left < j)
+            {
+                if (!SortShuffle(a, left, j, cis, local))
+                    return false;
+            }
+            // continue sort on the right partition
+            if (i < right)
+            {
+                if (!SortShuffle(a, i, right, cis, local))
+                    return false;
+            }
+            return true;
         }
     }
 }
