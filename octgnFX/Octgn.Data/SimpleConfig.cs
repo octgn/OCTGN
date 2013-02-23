59c23e2c8c08ca5dfd0044f2704e9e8378906b3a
Uri <sakura256@gmail.com>
2013-02-23 02:43:05 +0100

    config file locking

--- octgnFX/Octgn.Data/SimpleConfig.cs
+++ octgnFX/Octgn.Data/SimpleConfig.cs
@@ -9,6 +9,8 @@ namespace Octgn.Data
 {
     public static class SimpleConfig
     {
+        private static object lockObject = new Object();
+
         /// <summary>
         /// Special case since it's required in Octgn.Data, and Prefs can't go there
         /// </summary>
@@ -45,50 +47,55 @@ namespace Octgn.Data
         /// <returns> A string value </returns>
         public static string ReadValue(string valName)
         {
-            Stream f = null;
-            try
+            lock (lockObject)
             {
-                if (File.Exists(GetPath()))
-                {
-                    var serializer = new SharpSerializer();
-                    TryOpen(GetPath(), FileMode.OpenOrCreate, TimeSpan.FromSeconds(2), out f);
-                    var config = (Hashtable)serializer.Deserialize(f);
-                    if (config.ContainsKey(valName))
-                    {
-                        return config[valName].ToString();
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                Trace.WriteLine("[SimpleConfig]ReadValue Error: " + e.Message);
+                Stream f = null;
                 try
                 {
-                    File.Delete(GetPath());
+                    if (File.Exists(GetPath()))
+                    {
+                        var serializer = new SharpSerializer();
+                        OpenFile(GetPath(), FileMode.OpenOrCreate, FileShare.None, TimeSpan.FromSeconds(2), out f);
+                        var config = (Hashtable)serializer.Deserialize(f);
+                        if (config.ContainsKey(valName))
+                        {
+                            return config[valName].ToString();
+                        }
+                    }
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
-                    Trace.WriteLine("[SimpleConfig]ReadValue Error: Couldn't delete the corrupt config file.");
+                    Trace.WriteLine("[SimpleConfig]ReadValue Error: " + e.Message);
+                    try
+                    {
+                        File.Delete(GetPath());
+                    }
+                    catch (Exception)
+                    {
+                        Trace.WriteLine("[SimpleConfig]ReadValue Error: Couldn't delete the corrupt config file.");
+                    }
                 }
-            }
-            finally
-            {
-                if (f != null)
+                finally
                 {
-                    UnlockStream(f);
-                    f.Close();
-                    f = null;
+                    if (f != null)
+                    {
+                        f.Close();
+                        f = null;
+                    }
                 }
+                return null;
             }
-            return null;
         }
 
         public static string ReadValue(string valName, string d)
         {
-            string r = ReadValue(valName);
-            if(r == null)
-                WriteValue(valName,d);
-            return r ?? d;
+            lock (lockObject)
+            {
+                string r = ReadValue(valName);
+                if (r == null)
+                    WriteValue(valName, d);
+                return r ?? d;
+            }
         }
 
         /// <summary>
@@ -98,86 +105,57 @@ namespace Octgn.Data
         /// <param name="value"> String to write for value </param>
         public static void WriteValue(string valName, string value)
         {
-            Stream f = null;
-            try
+            lock (lockObject)
             {
-                var serializer = new SharpSerializer();
-                var config = new Hashtable();
-                if (File.Exists(GetPath()))
+                Stream f = null;
+                try
                 {
-                    TryOpen(GetPath(), FileMode.OpenOrCreate, TimeSpan.FromSeconds(2), out f);
-                    LockStream(f);
-                    config = (Hashtable)serializer.Deserialize(f);
+                    var serializer = new SharpSerializer();
+                    var config = new Hashtable();
+                    if (File.Exists(GetPath()))
+                    {
+                        OpenFile(GetPath(), FileMode.OpenOrCreate, FileShare.None, TimeSpan.FromSeconds(2), out f);
+                        config = (Hashtable)serializer.Deserialize(f);
+                    }
+                    else
+                    {
+                        OpenFile(GetPath(), FileMode.OpenOrCreate, FileShare.None, TimeSpan.FromSeconds(2), out f);
+                    }
+                    config[valName] = value;
+                    serializer.Serialize(config, f);
                 }
-                else
+                catch (Exception e)
                 {
-                    TryOpen(GetPath(), FileMode.OpenOrCreate, TimeSpan.FromSeconds(2), out f);
-                    LockStream(f);
+                    Trace.WriteLine("[SimpleConfig]WriteValue Error: " + e.Message);
                 }
-                config[valName] = value;
-                serializer.Serialize(config, f);
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine("[SimpleConfig]WriteValue Error: " + e.Message);
-            }
-            finally
-            {
-                if (f != null)
+                finally
                 {
-                    UnlockStream(f);
-                    f.Close();
-                    f = null;
+                    if (f != null)
+                    {
+                        f.Close();
+                        f = null;
+                    }
                 }
             }
-
         }
 
-        private static void LockStream(Stream f)
-        {
-            FileStream file = (FileStream)f;
-            file.Lock(0, f.Length);
-        }
-
-        private static void UnlockStream(Stream f)
-        {
-            FileStream file = (FileStream)f;
-            file.Unlock(0, f.Length);
-        }
-
-        public static bool TryOpen(string path, FileMode fileMode, TimeSpan timeout, out Stream stream)
+        private static bool OpenFile(string path, FileMode fileMode, FileShare share, TimeSpan timeout, out Stream stream)
         {
             var endTime = DateTime.Now + timeout;
             while (DateTime.Now < endTime)
             {
-                if (TryOpen(path, fileMode, out stream))
+                try
+                {
+                    stream = File.Open(path, fileMode, FileAccess.ReadWrite, share);
                     return true;
+                }
+                catch (IOException e)
+                {
+                    //ignore this
+                }
             }
             stream = null;
             return false;
-        }
-
-        public static bool TryOpen(string path, FileMode fileMode, out Stream stream)
-        {
-            try
-            {
-                stream = File.Open(path, fileMode);
-                return true;
-            }
-            catch (IOException e)
-            {
-                if (!FileIsLocked(e))
-                    throw;
-
-                stream = null;
-                return false;
-            }
-        }
-
-        public static bool FileIsLocked(IOException ioException)
-        {
-            var errorCode = System.Runtime.InteropServices.Marshal.GetHRForException(ioException) & ((1 << 16) - 1);
-            return errorCode == 32 || errorCode == 33;
         }
     }
 }
