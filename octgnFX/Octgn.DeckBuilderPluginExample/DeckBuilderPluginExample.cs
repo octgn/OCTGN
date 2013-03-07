c5a07c8aff9e7aee6eec4bec49c68c7a80cb9937
Jenkins CI <octgngame@gmail.com>
2013-03-07 02:53:44 +0000

    Auto generated commit for version 3.0.11.56

--- octgnFX/Octgn.DeckBuilderPluginExample/DeckBuilderPluginExample.cs
+++ octgnFX/Octgn.DeckBuilderPluginExample/DeckBuilderPluginExample.cs
@@ -59,8 +59,8 @@ namespace Octgn.DeckBuilderPluginExample
         {
             get
             {
-                // Don't allow this plugin to be used in any version less than 3.0.10.56
-                return Version.Parse("3.0.10.56");
+                // Don't allow this plugin to be used in any version less than 3.0.11.56
+                return Version.Parse("3.0.11.56");
             }
         }
     }
