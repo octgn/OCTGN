2568dfac62e9367677849f269be67a0cd0e66f8a
Uri <sakura256@gmail.com>
2013-03-17 04:26:13 +0100

    Reworked proxygenerator

--- octgnFX/Octgn.ProxyGenerator/Structs/SectionStructs.cs
+++ octgnFX/Octgn.ProxyGenerator/Structs/SectionStructs.cs
@@ -4,7 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 
-namespace Octgn.ProxyGenerator
+namespace Octgn.ProxyGenerator.Structs
 {
     public class SectionStructs
     {
---
+++ octgnFX/Octgn.ProxyGenerator/TemplateSelector.cs
@@ -0,0 +1,60 @@
+﻿using Octgn.ProxyGenerator.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Octgn.ProxyGenerator
+{
+    public class TemplateSelector
+    {
+        private List<CardDefinition> templates = null;
+
+        public string DefaultID { get; set; }
+
+        public TemplateSelector()
+        {
+            templates = new List<CardDefinition>();
+        }
+
+        public void AddTemplate(CardDefinition cardDef)
+        {
+            if (templates.Contains(cardDef))
+            {
+                templates.Remove(cardDef);
+            }
+            templates.Add(cardDef);
+        }
+
+        public CardDefinition GetTemplate(string ID)
+        {
+            CardDefinition ret = GetDefaultTemplate();
+            foreach (CardDefinition cardDef in templates)
+            {
+                if (cardDef.id == ID)
+                {
+                    ret = cardDef;
+                    break;
+                }
+            }
+            return (ret);
+        }
+
+        public CardDefinition GetDefaultTemplate()
+        {
+            foreach (CardDefinition cardDef in templates)
+            {
+                if (cardDef.id == DefaultID)
+                {
+                    return cardDef;
+                }
+            }
+            return null;
+        }
+
+        public void ClearTemplates()
+        {
+            templates.Clear();
+        }
+    }
+}
