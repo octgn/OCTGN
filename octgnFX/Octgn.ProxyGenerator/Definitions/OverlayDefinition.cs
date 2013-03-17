45b04b13ab3120aa115aaa1143ba33de31ce034c
Uri <sakura256@gmail.com>
2013-03-17 04:26:13 +0100

    Reworked proxygenerator

--- octgnFX/Octgn.ProxyGenerator/Definitions/OverlayDefinition.cs
+++ octgnFX/Octgn.ProxyGenerator/Definitions/OverlayDefinition.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Octgn.ProxyGenerator.Structs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
 
-namespace Octgn.ProxyGenerator
+namespace Octgn.ProxyGenerator.Definitions
 {
     public class OverlayDefinition
     {
