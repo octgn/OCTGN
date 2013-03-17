f7d7c6f4fd5b8f9a78384fda1ba301b5272e3549
Kelly Elton <kelly.elton@skylabsonline.com>
2013-03-16 20:43:43 -0500

    Deck save implemented.

--- octgnFX/Octgn.Core/DataExtensionMethods/DeckExtensionMethods.cs
+++ octgnFX/Octgn.Core/DataExtensionMethods/DeckExtensionMethods.cs
@@ -1,21 +1,68 @@
 ﻿namespace Octgn.Core.DataExtensionMethods
 {
     using System;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
+    using System.Xml;
 
     using Octgn.DataNew.Entities;
+    using Octgn.Library.Exceptions;
+
+    using log4net;
 
     public static class DeckExtensionMethods
     {
-         public static void Save(this IDeck deck, string path)
-         {
-             //TODO [DB MIGRATION] DO THIS!
-             throw new NotImplementedException("OH GODS");
-         }
+        internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public static void Save(this IDeck deck, Game game, string path)
+        {
+            try
+            {
+                using (var fs = File.Open(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                {
+                    var writer = XmlWriter.Create(fs);
+                    writer.WriteStartDocument(true);
+                    writer.WriteStartElement("deck");
+                    writer.WriteAttributeString("game",game.Id.ToString());
+                    foreach (var section in deck.Sections)
+                    {
+                        writer.WriteStartElement("section");
+                        writer.WriteAttributeString("name",section.Name);
+                        foreach (var c in section.Cards)
+                        {
+                            writer.WriteStartElement("card");
+                            writer.WriteAttributeString("qty",c.Quantity.ToString());
+                            writer.WriteAttributeString("id",c.Id.ToString());
+                            writer.WriteString(c.Name);
+                            writer.WriteEndElement();
+                        }
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+            catch (PathTooLongException)
+            {
+                throw new UserMessageException("Could not save deck to {0}, the file path would be too long.",path);
+            }
+            catch (IOException e)
+            {
+                Log.Error(String.Format("Problem saving deck to path {0}", path), e);
+                throw new UserMessageException("Could not save deck to {0}, {1}", path,e.Message);
+            }
+            catch (Exception e)
+            {
+                Log.Error(String.Format("Problem saving deck to path {0}",path),e);
+                throw new UserMessageException("Could not save deck to {0}, there was an unspecified problem.",path);
+            }
+        }
         public static IDeck Load(this IDeck deck, string path)
         {
             //TODO [DB MIGRATION]  DO THIS!
-             throw new NotImplementedException("OH GODS");
+            throw new NotImplementedException("OH GODS");
             return new Deck();
         }
         public static int CardCount(this IDeck deck)
