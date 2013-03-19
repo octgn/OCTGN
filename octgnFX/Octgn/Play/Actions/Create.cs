9125ecd9d6c1368fa4f787e0dc403e27c965f755
Kelly Elton <kelly.elton@skylabsonline.com>
2013-03-18 21:39:51 -0500

    Octgn.Play.Actions.CreateCard is now compile error free.

--- octgnFX/Octgn/Play/Actions/Create.cs
+++ octgnFX/Octgn/Play/Actions/Create.cs
@@ -35,7 +35,7 @@ namespace Octgn.Play.Actions
             base.Do();
 
             Card =
-                new Card(_owner, _id, _key, Program.GameEngine.Definition.CardDefinition, _model, false)
+                new Card(_owner, _id, _key, _model, false)
                     {X = _x, Y = _y, DeleteWhenLeavesGroup = _deletesWhenLeavesGroup};
             Card.SetFaceUp(_faceUp);
             Program.GameEngine.Table.AddAt(Card, Program.GameEngine.Table.Count);
