27e8f256631ebc340fe080367f05ba75b987ab36
Kelly Elton <kelly.elton@skylabsonline.com>
2013-03-21 22:43:57 -0500

    Started some game feed code.

---
+++ octgnFX/Octgn.Core/GameFeedManager.cs
@@ -0,0 +1,103 @@
+﻿namespace Octgn.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Timers;
+
+    using NuGet;
+
+    using log4net;
+
+    public class GameFeedManager : IDisposable
+    {
+        #region Singleton
+
+        internal static GameFeedManager SingletonContext { get; set; }
+
+        private static readonly object GameFeedManagerSingletonLocker = new object();
+
+        public static GameFeedManager Get()
+        {
+            lock (GameFeedManagerSingletonLocker) return SingletonContext ?? (SingletonContext = new GameFeedManager());
+        }
+        internal GameFeedManager()
+        {
+            
+        }
+        #endregion Singleton
+
+        internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        internal string MainFeed = "http://www.myget.org/F/octgngames/";
+        internal int RefreshTime = 10 * 60 * 1000; //Believe that's 10 minutes
+
+        internal bool IsRunning { get; set; }
+        internal Timer RefreshTimer { get; set; }
+
+        #region StartStop
+        public void Start()
+        {
+            if (IsRunning) return;
+            IsRunning = true;
+            this.ConstructTimer();
+            this.RefreshTimerOnElapsed(null,null);
+        }
+
+        public void Stop()
+        {
+            if (!IsRunning) return;
+            IsRunning = false;
+            this.DestroyTimer();
+        }
+        #endregion StartStop
+
+        #region Timer
+        internal void ConstructTimer()
+        {
+            this.DestroyTimer();
+            RefreshTimer = new Timer(RefreshTime);
+            RefreshTimer.Elapsed += RefreshTimerOnElapsed;
+            RefreshTimer.Start();
+        }
+
+        internal void DestroyTimer()
+        {
+            if (RefreshTimer != null)
+            {
+                try { RefreshTimer.Elapsed -= this.RefreshTimerOnElapsed; }
+                catch { }
+                try { RefreshTimer.Stop(); }
+                catch { }
+                try { RefreshTimer.Dispose(); }
+                catch { }
+                RefreshTimer = null;
+            }
+        }
+        internal void RefreshTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+
+        }
+        #endregion Timer
+
+        public void CheckForUpdates()
+        {
+            foreach (var package in this.GetPackages())
+            {
+                //package.
+            }
+        }
+
+        internal IEnumerable<IPackage> GetPackages()
+        {
+            var repo = PackageRepositoryFactory.Default.CreateRepository(MainFeed);
+            var packages = repo.GetPackages().Where(x => x.IsAbsoluteLatestVersion);
+            return packages.ToList();
+        }
+
+        public void Dispose()
+        {
+            this.Stop();
+        }
+    }
+}
---
+++ octgnFX/Octgn.Test/Core/GameFeedManagerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Octgn.Test.Core
+{
+    using System;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using Octgn.Core;
+
+    public class GameFeedManagerTests
+    {
+        [NUnit.Framework.Test]
+        public void GetPackages_GetsOneRepo()
+        {
+            var ret = GameFeedManager.Get().GetPackages();
+            Assert.AreEqual(1, ret.Count());
+            Console.WriteLine(ret.First().Version.Version);
+        }
+    }
+}
