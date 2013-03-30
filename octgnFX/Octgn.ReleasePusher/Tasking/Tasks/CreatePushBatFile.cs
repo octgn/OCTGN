004e31ce4efdb56fd7eed2ad75521820975e6df3
Kelly Elton <kelly.elton@skylabsonline.com>
2013-03-30 14:59:50 -0500

    Updated to handle pushing to new test branch

--- octgnFX/Octgn.ReleasePusher/Tasking/Tasks/CreatePushBatFile.cs
+++ octgnFX/Octgn.ReleasePusher/Tasking/Tasks/CreatePushBatFile.cs
@@ -2,20 +2,37 @@
 {
     using System;
 
-    public class CreatePushBatFile: ITask
+    public class CreatePushBatFile : ITask
     {
         public void Run(object sender, ITaskContext context)
         {
-            const string BatTemplate = @"git commit -am ""Auto generated commit for version %v""
+
+            if ((context.Data["Mode"] as string).ToLower() == "release")
+            {
+                const string BatTemplate = @"git commit -am ""Auto generated commit for version %v""
 git tag -a %v -m ""Auto generated commit for version %v""
 git push origin HEAD:master
 git push --tags origin";
-            var file = context.Data["WorkingDirectory"] as string;
-            file = context.FileSystem.Path.Combine(file, "push.bat");
+                var file = context.Data["WorkingDirectory"] as string;
+                file = context.FileSystem.Path.Combine(file, "push.bat");
 
-            var outstr = BatTemplate.Replace("%v", (context.Data["NewVersion"] as Version).ToString());
+                var outstr = BatTemplate.Replace("%v", (context.Data["NewVersion"] as Version).ToString());
 
-            context.FileSystem.File.WriteAllText(file,outstr);
+                context.FileSystem.File.WriteAllText(file, outstr);
+            }
+            else if ((context.Data["Mode"] as string).ToLower() == "test")
+            {
+                const string BatTemplate = @"git commit -am ""Auto generated commit for version %v""
+git tag -a %v -m ""Auto generated commit for version %v""
+git push origin HEAD:test
+git push --tags origin";
+                var file = context.Data["WorkingDirectory"] as string;
+                file = context.FileSystem.Path.Combine(file, "push.bat");
+
+                var outstr = BatTemplate.Replace("%v", (context.Data["NewVersion"] as Version).ToString());
+
+                context.FileSystem.File.WriteAllText(file, outstr);
+            }
         }
     }
 }
