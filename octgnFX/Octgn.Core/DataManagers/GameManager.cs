53d77a610d95d6ddbc6bc46164d380b7ce168544
Kelly Elton <kelly.elton@skylabsonline.com>
2013-04-11 19:40:23 -0500

    delete all files in install directory before installing

--- octgnFX/Octgn.Core/DataManagers/GameManager.cs
+++ octgnFX/Octgn.Core/DataManagers/GameManager.cs
@@ -69,6 +69,18 @@
             var defPath = Path.Combine(dirPath, "def");
             if (!Directory.Exists(defPath)) return;
             var di = new DirectoryInfo(defPath);
+
+            // Try and delete the existing game first
+            try
+            {
+                var destPath = Path.Combine(Paths.Get().DataDirectory, "GameDatabase", package.Id);
+                Directory.Delete(destPath,true);
+            }
+            catch (Exception e)
+            {
+                Log.InfoFormat("Could not delete all files in install directory");
+            }
+
             foreach (var f in di.GetFiles("*",SearchOption.AllDirectories))
             {
                 var relPath = f.FullName.Replace(di.FullName, "");
