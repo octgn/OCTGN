bd185f8bef9dfbb5202acb0a03d8a80b79c3feb4
Jenkins CI <octgngame@gmail.com>
2013-04-26 17:21:09 +0000

    Auto generated commit for version 3.1.12.81

--- octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.1.12.80")]
-[assembly: AssemblyFileVersion("3.1.12.80")]
+[assembly: AssemblyVersion("3.1.12.81")]
+[assembly: AssemblyFileVersion("3.1.12.81")]
--- octgnFX/Octgn/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Windows;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.1.12.80")]
-[assembly: AssemblyFileVersion("3.1.12.80")]
+[assembly: AssemblyVersion("3.1.12.81")]
+[assembly: AssemblyFileVersion("3.1.12.81")]
