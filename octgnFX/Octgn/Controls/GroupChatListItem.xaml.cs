a9c40f4c42a31b2cf3b0c9593de59ad10f80070f
Kelly Elton <kelly.elton@skylabsonline.com>
2013-05-06 21:57:37 -0500

    Contact list a little cleaner bigger window size

--- octgnFX/Octgn/Controls/GroupChatListItem.xaml.cs
+++ octgnFX/Octgn/Controls/GroupChatListItem.xaml.cs
@@ -6,23 +6,34 @@ using Skylabs.Lobby;
 
 namespace Octgn.Controls
 {
+    using System.ComponentModel;
+
+    using Octgn.Annotations;
+
     /// <summary>
     ///   Interaction logic for FriendListItem.xaml
     /// </summary>
-    public partial class GroupChatListItem
+    public partial class GroupChatListItem : INotifyPropertyChanged
     {
         private bool _isRemoving = false;
 
-        public string UserName { get
+        public string UserName
         {
-            return (string)this.GetValue(UsernameProperty);
-        } set
-        {
-            this.SetValue(UsernameProperty,value);
-        } }
+            get
+            {
+                return (string)this.GetValue(UsernameProperty);
+            }
+            set
+            {
+                this.SetValue(UsernameProperty, value);
+            }
+        }
 
         public static DependencyProperty UsernameProperty = DependencyProperty.Register(
-            "UserName", typeof (string), typeof (GroupChatListItem));
+            "UserName", typeof(string), typeof(GroupChatListItem));
+
+        public static DependencyProperty SelectedProperty = DependencyProperty.Register(
+            "Selected", typeof(bool), typeof(GroupChatListItem));
 
         private long _chatRoomId;
 
@@ -32,6 +43,13 @@ namespace Octgn.Controls
         {
             InitializeComponent();
             ThisRoom = null;
+            this.GotFocus += OnGotFocus;
+            this.LostFocus += OnLostFocus;
+            this.Unloaded += (sender, args) =>
+                {
+                    this.GotFocus -= this.OnGotFocus;
+                    this.LostFocus -= this.OnLostFocus;
+                };
         }
 
         public ChatRoom ThisRoom
@@ -54,6 +72,19 @@ namespace Octgn.Controls
             }
         }
 
+        public bool Selected
+        {
+            get
+            {
+                return (bool)this.GetValue(SelectedProperty);
+            }
+            set
+            {
+                this.SetValue(SelectedProperty, value);
+                OnPropertyChanged("Selected");
+            }
+        }
+
         private void UserControlMouseDown(object sender, MouseButtonEventArgs e)
         {
             //Focus();
@@ -61,15 +92,38 @@ namespace Octgn.Controls
 
         private void FlistitemMouseUp(object sender, MouseButtonEventArgs e)
         {
-            if(!_isRemoving)
+            if (!_isRemoving)
                 Focus();
         }
 
         private void Image1MouseUp(object sender, MouseButtonEventArgs e)
         {
             if (_chatRoom.GroupUser.UserName == "lobby") return;
-			_chatRoom.LeaveRoom();
+            _chatRoom.LeaveRoom();
             _isRemoving = true;
+        }
+
+
+        private void OnLostFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Selected = false;
+        }
+
+        private void OnGotFocus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Selected = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
