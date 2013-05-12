1ed0d8a5034d99ca0ad1a19f3346d26eb667ccce
Uri <sakura256@gmail.com>
2013-05-12 10:48:34 +0200

    changed validation order. added duplicated property checking for gamedef.

--- octgnFX/o8build/GameValidator.cs
+++ octgnFX/o8build/GameValidator.cs
@@ -67,6 +67,196 @@
                 foreach (var dir in setDir.GetDirectories())
                 {
                     this.VerifySetDirectory(dir);
+                }
+            }
+        }
+
+        [GameValidatorAttribute]
+        public void VerifyDef()
+        {
+            var libAss = Assembly.GetAssembly(typeof(Paths));
+            var gamexsd = libAss.GetManifestResourceNames().FirstOrDefault(x => x.Contains("Game.xsd"));
+            if (gamexsd == null)
+                throw new UserMessageException("Shits fucked bro.");
+            var schemas = new XmlSchemaSet();
+            var schema = XmlSchema.Read(libAss.GetManifestResourceStream(gamexsd), (sender, args) => { throw args.Exception; });
+            schemas.Add(schema);
+
+            var fileName = Directory.GetFiles().First(x => x.Name == "definition.xml").FullName;
+            XDocument doc = XDocument.Load(fileName);
+            string msg = "";
+            doc.Validate(schemas, (o, e) =>
+            {
+                msg = e.Message;
+            });
+            if (!string.IsNullOrWhiteSpace(msg))
+                throw new UserMessageException(msg);
+        }
+
+        [GameValidatorAttribute]
+        public void VerifyDefPaths()
+        {
+            const string gError = "{0} {1} does not exist here {1}. Remember paths cannot start with / or \\";
+            XmlSerializer serializer = new XmlSerializer(typeof(game));
+            var fs = File.Open(Directory.GetFiles().First(x => x.Name == "definition.xml").FullName, FileMode.Open);
+            var game = (game)serializer.Deserialize(fs);
+            fs.Close();
+            var path = "";
+
+            foreach (var s in game.scripts)
+            {
+                path = Path.Combine(Directory.FullName, s.src.Trim('\\', '/'));
+                if (!File.Exists(path))
+                    throw new UserMessageException(gError, "Script file", s.src, path);
+            }
+
+            if (game.fonts != null)
+            {
+                foreach (var font in game.fonts)
+                {
+                    path = Path.Combine(Directory.FullName, font.src);
+                    if (!File.Exists(path))
+                        throw new UserMessageException(gError, "Font", font.src, path);
+                }
+            }
+
+            if (game.documents != null)
+            {
+                foreach (var doc in game.documents)
+                {
+                    path = Path.Combine(Directory.FullName, doc.src);
+                    if (!File.Exists(path))
+                        throw new UserMessageException(gError, "Document", doc.src, path);
+                    path = Path.Combine(Directory.FullName, doc.icon);
+                    if (!File.Exists(path))
+                        throw new UserMessageException(gError, "Document", doc.icon, path);
+
+                }
+            }
+            if (game.proxygen != null)
+            {
+                path = Path.Combine(Directory.FullName, game.proxygen.definitionsrc);
+                if (!File.Exists(path))
+                    throw new UserMessageException(gError, "ProxyGen", game.proxygen.definitionsrc, path);
+            }
+            path = Path.Combine(Directory.FullName, game.card.front);
+            if (!File.Exists(path))
+                throw new UserMessageException(gError, "Card front", game.card.front, path);
+            path = Path.Combine(Directory.FullName, game.card.back);
+            if (!File.Exists(path))
+                throw new UserMessageException(gError, "Card back", game.card.back, path);
+
+            if (game.card.property.Length > 0)
+            {
+                List<string> props = new List<string>();
+                foreach (var prop in game.card.property)
+                {
+                    if (!props.Contains(prop.name))
+                    {
+                        props.Add(prop.name);
+                    }
+                    else
+                    {
+                        throw new UserMessageException("Duplicate property defined named {0} in file: {1}", prop.name, Path.Combine(Directory.FullName, "defintion.xml"));
+                    }
+                }
+                props.Clear();
+            }
+
+            if (!string.IsNullOrWhiteSpace(game.table.board))
+            {
+                path = Path.Combine(Directory.FullName, game.table.board);
+                if (!File.Exists(path)) throw new UserMessageException(gError, "Table board", game.table.board, path);
+            }
+
+            path = Path.Combine(Directory.FullName, game.table.background);
+            if (!File.Exists(path))
+                throw new UserMessageException(gError, "Table background", game.table.background, path);
+            if (game.player != null)
+            {
+                foreach (var counter in game.player.Items.OfType<counter>())
+                {
+                    path = Path.Combine(Directory.FullName, counter.icon);
+                    if (!File.Exists(path))
+                        throw new UserMessageException(gError, "Counter icon", counter.icon, path);
+                }
+                foreach (var hand in game.player.Items.OfType<group>())
+                {
+                    path = Path.Combine(Directory.FullName, hand.icon);
+                    if (!File.Exists(path))
+                        throw new UserMessageException(gError, "Group " + hand.name, hand.icon, path);
+                }
+            }
+            if (game.shared != null)
+            {
+                if (game.shared.counter != null)
+                {
+                    foreach (var counter in game.shared.counter)
+                    {
+                        path = Path.Combine(Directory.FullName, counter.icon);
+                        if (!File.Exists(path)) throw new UserMessageException(gError, "Counter icon", counter.icon, path);
+                    }
+                }
+                if (game.shared.group != null)
+                {
+                    foreach (var hand in game.shared.group)
+                    {
+                        path = Path.Combine(Directory.FullName, hand.icon);
+                        if (!File.Exists(path)) throw new UserMessageException(gError, "Group " + hand.name, hand.icon, path);
+                    }
+                }
+            }
+        }
+
+        [GameValidatorAttribute]
+        public void VerifyScripts()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(game));
+            var fs = File.Open(Directory.GetFiles().First(x => x.Name == "definition.xml").FullName, FileMode.Open);
+            var game = (game)serializer.Deserialize(fs);
+            fs.Close();
+
+            var engine = Python.CreateEngine(AppDomain.CurrentDomain);
+
+            var errorList = new List<string>();
+
+            foreach (var script in game.scripts)
+            {
+                var scr = script;
+                var ss = engine.CreateScriptSourceFromFile(Path.Combine(Directory.FullName, scr.src));
+                ss.Compile(new CompileErrorListener((source, message, span, code, severity) =>
+                {
+                    var errString = String.Format(
+                        "[{0} {1}-{2}:{3} to {4}:{5}] {6} - {7}",
+                        severity.ToString(),
+                        code,
+                        span.Start.Line,
+                        span.Start.Column,
+                        span.End.Line,
+                        span.End.Column,
+                        scr.src,
+                        message);
+                    errorList.Add(errString);
+                }));
+            }
+
+            var sb = new StringBuilder("==Script Errors==");
+            foreach (var err in errorList)
+            {
+                sb.AppendLine(err);
+            }
+            if (errorList.Count > 0)
+                throw new UserMessageException(sb.ToString());
+        }
+
+        [GameValidatorAttribute]
+        public void TestSetXml()
+        {
+            if (Directory.GetDirectories().Any(x => x.Name == "Sets"))
+            {
+                var setDir = Directory.GetDirectories().First(x => x.Name == "Sets");
+                foreach (var dir in setDir.GetDirectories())
+                {
                     var setFile = dir.GetFiles().First();
                     TestSetXml(setFile.FullName);
                     CheckSetXML(setFile.FullName);
@@ -189,166 +379,6 @@
             doc = null;
         }
 
-        [GameValidatorAttribute]
-        public void VerifyDef()
-        {
-            var libAss = Assembly.GetAssembly(typeof(Paths));
-            var gamexsd = libAss.GetManifestResourceNames().FirstOrDefault(x => x.Contains("Game.xsd"));
-            if (gamexsd == null)
-                throw new UserMessageException("Shits fucked bro.");
-            var schemas = new XmlSchemaSet();
-            var schema = XmlSchema.Read(libAss.GetManifestResourceStream(gamexsd), (sender, args) =>{ throw args.Exception; });
-            schemas.Add(schema);
-
-            var fileName = Directory.GetFiles().First(x=>x.Name == "definition.xml").FullName;
-            XDocument doc = XDocument.Load(fileName);
-            string msg = "";
-            doc.Validate(schemas, (o, e) =>
-            {
-                msg = e.Message;
-            });
-            if(!string.IsNullOrWhiteSpace(msg))
-                throw new UserMessageException(msg);
-        }
-
-        [GameValidatorAttribute]
-        public void VerifyDefPaths()
-        {
-            const string gError = "{0} {1} does not exist here {1}. Remember paths cannot start with / or \\";
-            XmlSerializer serializer = new XmlSerializer(typeof(game));
-            var fs = File.Open(Directory.GetFiles().First(x=>x.Name == "definition.xml").FullName, FileMode.Open);
-            var game = (game)serializer.Deserialize(fs);
-            fs.Close();
-            var path = "";
-
-            foreach (var s in game.scripts)
-            {
-                path = Path.Combine(Directory.FullName,s.src.Trim('\\','/'));
-                if(!File.Exists(path))
-                    throw new UserMessageException(gError,"Script file",s.src,path);
-            }
-
-            if (game.fonts != null)
-            {
-                foreach (var font in game.fonts)
-                {
-                    path = Path.Combine(Directory.FullName, font.src);
-                    if (!File.Exists(path))
-                        throw new UserMessageException(gError, "Font", font.src, path);
-                }
-            }
-
-            if (game.documents != null)
-            {
-                foreach (var doc in game.documents)
-                {
-                    path = Path.Combine(Directory.FullName, doc.src);
-                    if(!File.Exists(path))
-                        throw new UserMessageException(gError,"Document",doc.src,path);
-                    path = Path.Combine(Directory.FullName, doc.icon);
-                    if(!File.Exists(path))
-                        throw new UserMessageException(gError,"Document",doc.icon,path);
-
-                }
-            }
-            if (game.proxygen != null)
-            {
-                path = Path.Combine(Directory.FullName, game.proxygen.definitionsrc);
-                if(!File.Exists(path))
-                    throw new UserMessageException(gError, "ProxyGen", game.proxygen.definitionsrc, path);
-            }
-            path = Path.Combine(Directory.FullName, game.card.front);
-            if(!File.Exists(path))
-                throw new UserMessageException(gError,"Card front",game.card.front,path);
-            path = Path.Combine(Directory.FullName, game.card.back);
-            if(!File.Exists(path))
-                throw new UserMessageException(gError,"Card back", game.card.back, path);
-
-            if (!string.IsNullOrWhiteSpace(game.table.board))
-            {
-                path = Path.Combine(Directory.FullName, game.table.board);
-                if (!File.Exists(path)) throw new UserMessageException(gError, "Table board", game.table.board, path);
-            }
-
-            path = Path.Combine(Directory.FullName, game.table.background);
-            if(!File.Exists(path))
-                throw new UserMessageException(gError,"Table background",game.table.background,path);
-            if (game.player != null)
-            {
-                foreach (var counter in game.player.Items.OfType<counter>())
-                {
-                    path = Path.Combine(Directory.FullName, counter.icon);
-                    if(!File.Exists(path))
-                        throw new UserMessageException(gError,"Counter icon",counter.icon,path);
-                }
-                foreach (var hand in game.player.Items.OfType<group>())
-                {
-                    path = Path.Combine(Directory.FullName, hand.icon);
-                    if(!File.Exists(path))
-                        throw new UserMessageException(gError,"Group " + hand.name,hand.icon,path);
-                }
-            }
-            if (game.shared!= null)
-            {
-                if (game.shared.counter != null)
-                {
-                    foreach (var counter in game.shared.counter)
-                    {
-                        path = Path.Combine(Directory.FullName, counter.icon);
-                        if (!File.Exists(path)) throw new UserMessageException(gError, "Counter icon", counter.icon, path);
-                    }
-                }
-                if (game.shared.group != null)
-                {
-                    foreach (var hand in game.shared.group)
-                    {
-                        path = Path.Combine(Directory.FullName, hand.icon);
-                        if (!File.Exists(path)) throw new UserMessageException(gError, "Group " + hand.name, hand.icon, path);
-                    }
-                }
-            }
-        }
-
-        [GameValidatorAttribute]
-        public void VerifyScripts()
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(game));
-            var fs = File.Open(Directory.GetFiles().First(x => x.Name == "definition.xml").FullName, FileMode.Open);
-            var game = (game)serializer.Deserialize(fs);
-            fs.Close();
-
-            var engine = Python.CreateEngine(AppDomain.CurrentDomain);
-
-            var errorList = new List<string>();
-
-            foreach (var script in game.scripts)
-            {
-                var scr = script;
-                var ss = engine.CreateScriptSourceFromFile(Path.Combine(Directory.FullName,scr.src));
-                ss.Compile(new CompileErrorListener((source, message, span, code, severity) =>
-                    {
-                        var errString = String.Format(
-                            "[{0} {1}-{2}:{3} to {4}:{5}] {6} - {7}",
-                            severity.ToString(),
-                            code,
-                            span.Start.Line,
-                            span.Start.Column,
-                            span.End.Line,
-                            span.End.Column,
-                            scr.src,
-                            message);
-                        errorList.Add(errString);
-                    }));
-            }
-
-            var sb = new StringBuilder("==Script Errors==");
-            foreach (var err in errorList)
-            {
-                sb.AppendLine(err);
-            }
-            if (errorList.Count > 0)
-                throw new UserMessageException(sb.ToString());
-        }
 
         [GameValidatorAttribute]
         public void VerifyProxyDef()
