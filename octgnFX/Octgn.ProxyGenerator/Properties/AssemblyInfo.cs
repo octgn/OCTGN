62c270cd778e2ac3b2203efa8d2f1291ff95b224
Jenkins CI <octgngame@gmail.com>
2013-05-29 03:13:17 +0000

    Auto generated commit for version 3.1.36.129-Test

--- octgnFX/Octgn.ProxyGenerator/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.ProxyGenerator/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.36.128")]
-[assembly: AssemblyFileVersion("3.1.36.128")]
+[assembly: AssemblyVersion("3.1.36.129")]
+[assembly: AssemblyFileVersion("3.1.36.129")]
