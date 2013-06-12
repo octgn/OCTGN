f5a4e455dfcebd0a3bfb1242233d18f5f0b272da
Kelly Elton <kelly.elton@skylabsonline.com>
2013-06-12 15:06:53 -0500

    Few more spot fixes.

--- octgnFX/Octgn.LobbyServer/Gaming.cs
+++ octgnFX/Octgn.LobbyServer/Gaming.cs
@@ -7,16 +7,17 @@ using Skylabs.Lobby;
 namespace Skylabs.LobbyServer
 {
     using System.IO;
+    using System.Threading.Tasks;
 
     public static class Gaming
     {
         //private static readonly object GamingLocker = new object();
         private static int _currentHostPort = 10000;
-        private static readonly ReaderWriterLockSlim Locker; 
+        private static readonly ReaderWriterLockSlim Locker;
 
         static Gaming()
         {
-            Locker = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+            Locker = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
             Games = new Dictionary<int, HostedGame>();
         }
 
@@ -25,11 +26,11 @@ namespace Skylabs.LobbyServer
         public static void Stop()
         {
             Locker.EnterWriteLock();
-                foreach (var g in Games)
-                {
-                    g.Value.Stop();
-                }
-                Games.Clear();
+            foreach (var g in Games)
+            {
+                g.Value.Stop();
+            }
+            Games.Clear();
             Locker.ExitWriteLock();
         }
 
@@ -42,31 +43,37 @@ namespace Skylabs.LobbyServer
                 if (_currentHostPort >= 20000)
                     _currentHostPort = 10000;
             }
-            var hs = new HostedGame(_currentHostPort, g, v,"unknown", name, pass, u,true,true);
-            hs.HostedGameDone += HostedGameExited;
+            var hs = new HostedGame(_currentHostPort, g, v, "unknown", name, pass, u, true, true);
+            hs.HostedGameDone += HostedGameExitedEventLauncher;
             if (hs.StartProcess())
             {
                 Games.Add(_currentHostPort, hs);
                 Locker.ExitWriteLock();//Exit Lock
                 return _currentHostPort;
             }
-            hs.HostedGameDone -= HostedGameExited;
+            hs.HostedGameDone -= HostedGameExitedEventLauncher;
             Locker.ExitWriteLock();//Exit Lock
             return -1;
 
         }
 
+        private static void HostedGameExitedEventLauncher(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(
+                () => HostedGameExited(sender, e));
+        }
+
         public static void StartGame(int port)
         {
             Locker.EnterWriteLock();
-                try
-                {
-                    Games[port].Status = Lobby.EHostedGame.GameInProgress;
-                }
-                catch (Exception e)
-                {
-                    Logger.Er(e);
-                }
+            try
+            {
+                Games[port].Status = Lobby.EHostedGame.GameInProgress;
+            }
+            catch (Exception e)
+            {
+                Logger.Er(e);
+            }
             Locker.ExitWriteLock();
         }
 
@@ -76,9 +83,8 @@ namespace Skylabs.LobbyServer
             List<Lobby.HostedGameData> sendgames =
                 Games.Select(
                     g =>
-                    new Lobby.HostedGameData(g.Value.GameGuid, (Version) g.Value.GameVersion.Clone(), g.Value.Port,
-                                            (string) g.Value.Name.Clone(), (User) g.Value.Hoster, g.Value.TimeStarted,!String.IsNullOrWhiteSpace(g.Value.Password))
-                        {GameStatus = g.Value.Status}).ToList();
+                    new Lobby.HostedGameData(g.Value.GameGuid, (Version)g.Value.GameVersion.Clone(), g.Value.Port,
+                                            (string)g.Value.Name.Clone(), (User)g.Value.Hoster, g.Value.TimeStarted, !String.IsNullOrWhiteSpace(g.Value.Password)) { GameStatus = g.Value.Status }).ToList();
             Locker.ExitReadLock();
             return sendgames;
         }
@@ -86,14 +92,15 @@ namespace Skylabs.LobbyServer
         private static void HostedGameExited(object sender, EventArgs e)
         {
             Locker.EnterWriteLock();
-                var s = sender as HostedGame;
-                if (s == null)
-                {
-                    Locker.ExitWriteLock();
-                    return;
-                }
+            var s = sender as HostedGame;
+            if (s == null)
+            {
+                Locker.ExitWriteLock();
+                return;
+            }
             try
             {
+                s.HostedGameDone -= HostedGameExitedEventLauncher;
                 var dir = new FileInfo(s.StandAloneApp.StartInfo.FileName).Directory;
                 dir.Delete(true);
             }
@@ -101,8 +108,8 @@ namespace Skylabs.LobbyServer
             {
                 Logger.Er(ex);
             }
-                s.Status = Lobby.EHostedGame.StoppedHosting;
-                Games.Remove(s.Port);
+            s.Status = Lobby.EHostedGame.StoppedHosting;
+            Games.Remove(s.Port);
             Locker.ExitWriteLock();
         }
     }
