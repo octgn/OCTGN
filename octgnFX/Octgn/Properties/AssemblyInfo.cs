e154c578b91ddd78f4d3fcc717d720d27dab92b8
Jenkins CI <octgngame@gmail.com>
2013-07-08 02:05:37 +0000

    Auto generated commit for version 3.1.54.156-Test

--- octgnFX/Octgn/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.1.54.155")]
-[assembly: AssemblyFileVersion("3.1.54.155")]
+[assembly: AssemblyVersion("3.1.54.156")]
+[assembly: AssemblyFileVersion("3.1.54.156")]
