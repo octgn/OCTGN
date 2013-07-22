76455143a30f98c1d0f6ec7062935b0f11878459
Kelly Elton <kelly.elton@skylabsonline.com>
2013-07-22 14:52:44 -0500

    Started working on updating licenses

--- octgnFX/Octgn.Library/Utils/RegexPatterns.cs
+++ octgnFX/Octgn.Library/Utils/RegexPatterns.cs
@@ -1,11 +1,6 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="RegexPatterns.cs" company="OCTGN">
-//   GNU Stuff
-// </copyright>
-// <summary>
-//   Defines the RegexPatterns type.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 namespace Octgn.Library.Utils
 {
--- octgnFX/Octgn/Controls/ChatBar.cs
+++ octgnFX/Octgn/Controls/ChatBar.cs
@@ -1,11 +1,6 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ChatBar.cs" company="OCTGN">
-//   GNU Whatever
-// </copyright>
-// <summary>
-//   Defines the ChatBar type.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 namespace Octgn.Controls
 {
