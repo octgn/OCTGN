e032e53fcd7b84ade03aa33ed4332c875108ace3
Kelly Elton <kelly.elton@skylabsonline.com>
2013-08-08 14:44:41 -0500

    Starting work on UpdateBar.xaml

---
+++ octgnFX/Octgn/Controls/UpdateBar.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Octgn.Controls
+{
+    /// <summary>
+    /// Interaction logic for UpdateBar.xaml
+    /// </summary>
+    public partial class UpdateBar : INotifyPropertyChanged 
+    {
+        public string Message
+        {
+            get
+            {
+                return "Your version of OCTGN is out of date.";
+            }
+        }
+        public UpdateBar()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
