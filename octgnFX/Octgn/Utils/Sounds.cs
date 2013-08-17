c9082c531ffd9808eb8c557653fb61171135ad86
Kelly Elton <kelly.elton@skylabsonline.com>
2013-08-17 13:57:24 -0500

    Possibly fixed sound error.

--- octgnFX/Octgn/Utils/Sounds.cs
+++ octgnFX/Octgn/Utils/Sounds.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Media;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+using NAudio.Wave;
+
+using Octgn.DataNew.Entities;
+
+using log4net;
 
 namespace Octgn.Utils
 {
-    using System.Diagnostics;
-    using System.Reflection;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using System.Windows;
 
-    using NAudio.Wave;
-
-    using Octgn.DataNew.Entities;
-
-    using log4net;
-
-    public class Sounds
+    public static class Sounds
     {
         internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -27,7 +26,6 @@ namespace Octgn.Utils
             {
                 try
                 {
-                    using (sound)
                     using (var player = new SoundPlayer(sound))
                     {
                         player.PlaySync();
@@ -36,29 +34,14 @@ namespace Octgn.Utils
                 }
                 catch (Exception e)
                 {
-                    Log.Warn("Play Sound Error", e);
+                    Log.Warn("PlaySound Error", e);
                 }
             });
         }
 
         public static void PlaySound(string file, bool subRequired = true)
         {
-            if (subRequired && SubscriptionModule.Get().IsSubscribed == false) return;
-            Task.Factory.StartNew(() =>
-            {
-                try
-                {
-                    using (var player = new SoundPlayer(file))
-                    {
-                        player.PlaySync();
-                    }
-
-                }
-                catch (Exception e)
-                {
-                    Log.Warn("Play Sound Error", e);
-                }
-            });
+            PlaySound(File.OpenRead(file));
         }
 
         public static void PlayMessageSound()
@@ -82,30 +65,32 @@ namespace Octgn.Utils
             var isSubscribed = SubscriptionModule.Get().IsSubscribed ?? false;
             if (isSubscribed && Prefs.EnableGameSound)
             {
+                Log.InfoFormat("Playing game sound {0}", sound.Name);
+                if (!sound.Src.ToLowerInvariant().EndsWith(".mp3"))
+                {
+                    Log.InfoFormat("Playing game sound {0} as wav", sound.Name);
+                    PlaySound(sound.Src);
+                    return;
+                }
                 Task.Factory.StartNew(() =>
                 {
+
                     try
                     {
-                        if (sound.Src.ToLowerInvariant().EndsWith(".mp3"))
+                        Log.InfoFormat("Playing game sound {0} as mp3", sound.Name);
+                        using (var mp3Reader = new Mp3FileReader(sound.Src))
+                        using (var stream = new WaveChannel32(mp3Reader) { PadWithZeroes = false })
+                        using (var wo = new WaveOutEvent())
                         {
-                            using (var mp3Reader = new Mp3FileReader(sound.Src))
-                            using (var stream = new WaveChannel32(mp3Reader))
-                            using (var wo = new WaveOut())
+                            Log.InfoFormat("Initializing game sound {0} as mp3", sound.Name);
+                            wo.Init(stream);
+                            wo.Play();
+                            Log.InfoFormat("Waiting for game sound {0} to complete", sound.Name);
+                            while (wo.PlaybackState == PlaybackState.Playing)
                             {
-                                wo.Init(stream);
-                                wo.Play();
-                                while (wo.PlaybackState == PlaybackState.Playing)
-                                {
-                                    Thread.Sleep(1);
-                                }
+                                Thread.Sleep(1);
                             }
-                        }
-                        else
-                        {
-                            using (var player = new SoundPlayer(sound.Src))
-                            {
-                                player.PlaySync();
-                            }
+                            Log.InfoFormat("Game sound {0} completed", sound.Name);
                         }
                     }
                     catch (Exception e)
