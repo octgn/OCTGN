446b7e6fc88f03d17447c0aec91cbfa12464853e
Jenkins CI <octgngame@gmail.com>
2013-08-25 21:11:24 +0000

    Auto generated commit for version 3.1.76.187-Test

--- octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Server/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.1.76.186")]
-[assembly: AssemblyFileVersion("3.1.76.186")]
+[assembly: AssemblyVersion("3.1.76.187")]
+[assembly: AssemblyFileVersion("3.1.76.187")]
--- octgnFX/Skylabs.Lobby/Properties/AssemblyInfo.cs
+++ octgnFX/Skylabs.Lobby/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.76.186")]
-[assembly: AssemblyFileVersion("3.1.76.186")]
+[assembly: AssemblyVersion("3.1.76.187")]
+[assembly: AssemblyFileVersion("3.1.76.187")]
