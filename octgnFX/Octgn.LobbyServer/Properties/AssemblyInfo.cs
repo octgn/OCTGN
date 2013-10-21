be7dc83bef238247d46242d42c72407833553623
Kelly Elton <kelly.elton@skylabsonline.com>
2013-10-21 17:37:32 -0500

    New Version 3.1.97.227

--- octgnFX/Octgn.LobbyServer/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.LobbyServer/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.96.227")]
-[assembly: AssemblyFileVersion("3.1.96.227")]
+[assembly: AssemblyVersion("3.1.97.227")]
+[assembly: AssemblyFileVersion("3.1.97.227")]
