395b181c960663296894ff244cfff7aed1557de8
Jenkins CI <octgngame@gmail.com>
2013-11-17 21:12:27 +0000

    Auto generated commit for version 3.1.104.239-Test

--- octgnFX/Octgn.LobbyServer/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.LobbyServer/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.104.238")]
-[assembly: AssemblyFileVersion("3.1.104.238")]
+[assembly: AssemblyVersion("3.1.104.239")]
+[assembly: AssemblyFileVersion("3.1.104.239")]
