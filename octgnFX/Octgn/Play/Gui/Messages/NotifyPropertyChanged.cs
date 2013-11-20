6b97377e01d2d41ff4eeba38194816af636d5e53
Kelly Elton <kelly.elton@skylabsonline.com>
2013-11-19 19:51:32 -0600

    Added mvvm lite cause we may use it.

---
+++ octgnFX/Octgn/Play/Gui/Messages/NotifyPropertyChanged.cs
@@ -0,0 +1,17 @@
+﻿namespace Octgn.Play.Gui.Messages
+{
+    public abstract class NotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        protected NotifyPropertyChanged(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class GroupNotifyPropertyChanged
+    {
+        
+    }
+}
