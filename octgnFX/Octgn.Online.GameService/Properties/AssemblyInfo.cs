63e21f094f8cd8c88e13b1868662ea849e1c528a
Jenkins CI <octgngame@gmail.com>
2013-11-21 09:40:31 +0000

    Auto generated commit for Octgn.Online.GameService version 3.1.109.246

--- octgnFX/Octgn.Online.GameService/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Online.GameService/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0d677c51-4d4e-4ce2-a081-10495657b5a2")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
-[assembly: AssemblyVersion("3.1.109.245")]
-[assembly: AssemblyFileVersion("3.1.109.245")]
+[assembly: AssemblyVersion("3.1.109.246")]
+[assembly: AssemblyFileVersion("3.1.109.246")]
