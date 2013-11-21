08f187398a98fbc07d231e794dde15a383f435ff
Kelly Elton <kelly.elton@skylabsonline.com>
2013-11-21 02:54:02 -0600

    Removed some comments.

--- octgnFX/Octgn.Online.GameService/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Online.GameService/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Octgn.Online.GameService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,24 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0d677c51-4d4e-4ce2-a081-10495657b5a2")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1.109.244")]
 [assembly: AssemblyFileVersion("3.1.109.244")]
