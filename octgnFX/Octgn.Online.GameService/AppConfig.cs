7a20831c90c5265fc81ac119841fe8eb70a99ad7
Kelly Elton <kelly.elton@skylabsonline.com>
2013-11-20 21:09:00 -0600

    Piping in some gamebot plumbing into the new GameService

---
+++ octgnFX/Octgn.Online.GameService/AppConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+
+namespace Octgn.Online.GameService
+{
+    public class AppConfig
+    {
+        #region Singleton
+
+        internal static AppConfig SingletonContext { get; set; }
+
+        private static readonly object AppConfigSingletonLocker = new object();
+
+        public static AppConfig Instance
+        {
+            get
+            {
+                if (SingletonContext == null)
+                {
+                    lock (AppConfigSingletonLocker)
+                    {
+                        if (SingletonContext == null)
+                        {
+                            SingletonContext = new AppConfig();
+                        }
+                    }
+                }
+                return SingletonContext;
+            }
+        }
+
+        #endregion Singleton
+
+        public string ServerPath { get { return ConfigurationManager.AppSettings["ServerPath"]; } }
+
+        public string XmppUsername { get { return ConfigurationManager.AppSettings["XmppUsername"]; } }
+
+        public string XmppPassword { get { return ConfigurationManager.AppSettings["XmppPassword"]; } }
+    }
+}
