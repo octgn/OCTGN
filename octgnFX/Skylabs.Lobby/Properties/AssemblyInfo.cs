a2dfb1716d6edd36b8b5847460a6bb234b2fd92f
Jenkins CI <octgngame@gmail.com>
2013-11-24 09:54:31 +0000

    Auto generated commit for version 3.1.111.250

--- octgnFX/Skylabs.Lobby/Properties/AssemblyInfo.cs
+++ octgnFX/Skylabs.Lobby/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.110.250")]
-[assembly: AssemblyFileVersion("3.1.110.250")]
+[assembly: AssemblyVersion("3.1.111.250")]
+[assembly: AssemblyFileVersion("3.1.111.250")]
