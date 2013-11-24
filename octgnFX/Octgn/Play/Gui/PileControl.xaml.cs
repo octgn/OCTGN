87cf6e8665d471eb0302d678c2acfaa19c5c4bbd
Soul1355 <maiahee@gmail.com>
2013-11-24 01:35:29 -0800

    it wasn't actually doing anything...

--- octgnFX/Octgn/Play/Gui/PileControl.xaml.cs
+++ octgnFX/Octgn/Play/Gui/PileControl.xaml.cs
@@ -26,8 +26,6 @@ namespace Octgn.Play.Gui
         {
             base.GroupChanged();
             grid.ColumnDefinitions[0].Width = new GridLength(100*group.Def.Width/group.Def.Height);
-            // Fix for cardcontrol sometimes remaining hidden when cards are added to emty pile
-            if (group.Count > 0) cardsCtrl.Visibility = System.Windows.Visibility.Visible;
             var pile = (Pile) group;
             if (!pile.AnimateInsertion) return;
             pile.AnimateInsertion = false;
@@ -119,7 +117,6 @@ namespace Octgn.Play.Gui
                 cardsCtrl.Visibility = System.Windows.Visibility.Collapsed;
                 cardsCtrl.target.Visibility = System.Windows.Visibility.Collapsed;
             }
-            ;
         }
     }
 }
