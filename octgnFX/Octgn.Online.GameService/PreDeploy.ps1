051963d76157cb9e62cbd8d41b82b0c351389dff
Kelly Elton <kelly.elton@skylabsonline.com>
2014-01-19 00:56:41 -0600

    Crap

--- octgnFX/Octgn.Online.GameService/PreDeploy.ps1
+++ octgnFX/Octgn.Online.GameService/PreDeploy.ps1
@@ -1,7 +1,7 @@
 ﻿try
 {
-    $path= "C:\\Programming\\OCTGN\\octgnFX\\Octgn.Online.GameService\\bin\\Debug\\Octgn.Online.GameService.exe"
-	#$path = $OctopusParameters["OctopusOriginalPackageDirectoryPath"] + "/Octgn.Online.GameService.exe"
+    #$path= "C:\\Programming\\OCTGN\\octgnFX\\Octgn.Online.GameService\\bin\\Debug\\Octgn.Online.GameService.exe"
+	$path = $OctopusParameters["OctopusOriginalPackageDirectoryPath"] + "/Octgn.Online.GameService.exe"
 	Write-Host ("Exe Path: " + $path)
 
 	$startInfo = New-Object Diagnostics.ProcessStartInfo
