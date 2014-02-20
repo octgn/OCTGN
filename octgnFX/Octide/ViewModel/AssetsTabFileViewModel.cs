97c85da9ea0ac696466da8c1eaae1871f001e648
Kelly Elton <kelly.elton@skylabsonline.com>
2014-02-20 14:21:21 -0600

    Start of more asset work(basically just a few comments)

---
+++ octgnFX/Octide/ViewModel/AssetsTabFileViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Octide.ViewModel
+{
+    public abstract class AssetsTabFileViewModelBase : ViewModelBase
+    {
+        public string DisplayName
+        {
+            get { return displayName; }
+            set
+            {
+                if (value == displayName) return;
+                displayName = value;
+                RaisePropertyChanged("DisplayName");
+            }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                if (value == fileName) return;
+                fileName = value;
+                RaisePropertyChanged("FileName");
+            }
+        }
+
+        private string fileName;
+
+        private string displayName;
+
+		// Load files into array on start, and have tab control item source
+		// refelect current loaded ones or whatever...
+    }
+}
