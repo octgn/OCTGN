d96e40063b88bf3a9ab27e2db71960f22d0acd33
Kelly Elton <kelly.elton@skylabsonline.com>
2014-04-07 11:18:54 -0500

    Make octide update its version with everything else.

--- octgnFX/Octide/Properties/AssemblyInfo.cs
+++ octgnFX/Octide/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Octide")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright OCTGN ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.1.130.304")]
+[assembly: AssemblyFileVersion("3.1.130.304")]
