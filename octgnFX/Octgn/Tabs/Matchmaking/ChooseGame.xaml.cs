efa7d0cae3b35b02916a9ab923462d537a7a6dab
Kelly Elton <kelly.elton@skylabsonline.com>
2014-04-23 19:47:05 -0500

    Added game chooser. Doesn't really do anything yet but it looks legit.

---
+++ octgnFX/Octgn/Tabs/Matchmaking/ChooseGame.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Octgn.Core.DataManagers;
+using Octgn.DataNew.Entities;
+
+namespace Octgn.Tabs.Matchmaking
+{
+    public partial class ChooseGame
+    {
+        public ObservableCollection<Game> Games { get; set; }
+
+        public ChooseGame()
+        {
+			Games = new ObservableCollection<Game>();
+            InitializeComponent();
+			GameManager.Get().GameListChanged += OnGameListChanged;
+            rebuildList();
+        }
+
+        private void OnGameListChanged(object sender, EventArgs eventArgs)
+        {
+            rebuildList();
+        }
+
+        private void rebuildList()
+        {
+			if(Dispatcher.CheckAccess() == false)
+            {
+                Dispatcher.Invoke(new Action(rebuildList));
+                return;
+            }
+            var installedGames = GameManager.Get().Games.ToArray();
+            foreach (var g in installedGames)
+            {
+                if (Games.Any(x=>x.Id == g.Id))
+                    continue;
+				Games.Add(g);
+            }
+            foreach (var g in Games.ToArray())
+            {
+                if (installedGames.Any(x => x.Id == g.Id) == false)
+                {
+                    Games.Remove(g);
+                }
+            }
+        }
+    }
+}
