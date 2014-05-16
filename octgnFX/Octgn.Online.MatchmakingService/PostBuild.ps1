b101e05eb6aec348e992eb10c324dc82ef1170b8
Kelly Elton <kelly.elton@skylabsonline.com>
2014-05-15 20:48:03 -0500

    Got matchmaking server deploy stuff in place.

---
+++ octgnFX/Octgn.Online.MatchmakingService/PostBuild.ps1
@@ -0,0 +1,34 @@
+﻿function Push-Changes($version)
+{
+	$path = (Resolve-Path ".\octgnFX\Octgn.Online.Matchmaking")
+	Invoke-Expression "git add `"$path/*`""
+	Invoke-Expression "git commit -m `"Auto generated commit for Octgn.Online.Matchmaking version $version`""
+	Invoke-Expression "git tag -a $version -m `"Auto generated commit for Octgn.Online.Matchmaking version $version`""
+	Invoke-Expression "git push origin HEAD:test"
+	Invoke-Expression "git push --tags origin"
+}
+
+function Get-AssemblyInfoVersion($path)
+{
+	$rp = (Resolve-Path $path)
+	# Get the line containing the AssemblyVersion custom attribute
+	$attr = (get-content "$rp" | select-string "AssemblyVersion").ToString()
+
+	# Parse the attribute to get the 3 digit version
+	$s = $attr.IndexOf("`"")+1
+	$e = $attr.LastIndexOf("`"")
+	$version = $attr.Substring($s,$e-$s)
+	return $version
+}
+
+try
+{
+	$versionString = Get-AssemblyInfoVersion ".\octgnFX\Octgn.Online.Matchmaking\Properties\AssemblyInfo.cs"
+	$version = New-Object System.Version($versionString)
+	Push-Changes $version
+}
+catch
+{
+	Write-Host "Failed PostBuild: " $_
+	exit 1
+}
---
+++ octgnFX/Octgn.Online.MatchmakingService/PreDeploy.ps1
@@ -0,0 +1,23 @@
+﻿try
+{
+    #$path= "C:\\Programming\\OCTGN\\octgnFX\\Octgn.Online.Matchmaking\\bin\\Debug\\Octgn.Online.Matchmaking.exe"
+	$path = $OctopusParameters["OctopusOriginalPackageDirectoryPath"] + "/Octgn.Online.Matchmaking.exe"
+	Write-Host ("Exe Path: " + $path)
+
+	$startInfo = New-Object Diagnostics.ProcessStartInfo
+	$startInfo.Filename = $path
+	$startInfo.UseShellExecute = $false
+	$startInfo.Arguments = "kill";
+
+	Write-Host "Killing Process"
+	$Proc = [Diagnostics.Process]::Start($startInfo)
+	#$Proc = [Diagnostics.Process]::Start($path, "kill")
+
+	$Proc.WaitForExit()
+	Write-Host "Process Killed?"
+}
+catch
+{
+	Write-Host "Failed PreDeploy: " $_
+	exit 1
+}
