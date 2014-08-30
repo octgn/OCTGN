46b1d907a379d8931b92ecab3edbd36ab11f0127
Kelly Elton <kelly.elton@skylabsonline.com>
2014-08-30 13:49:01 -0500

    Some mouse over on the left menu bar

---
+++ octgnFX/Octgn/Controls/MenuBarButton.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Octgn.Controls
+{
+    public partial class MenuBarButton
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof (ImageSource), typeof (MenuBarButton), new PropertyMetadata(default(ImageSource)));
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource) GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof (string), typeof (MenuBarButton), new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public MenuBarButton()
+        {
+            InitializeComponent();
+        }
+
+        protected override Geometry GetLayoutClip(Size layoutSlotSize)
+        {
+            return ClipToBounds ? base.GetLayoutClip(layoutSlotSize) : null;
+        }
+    }
+}
