96630c49de2398f30a7a30399238f3f29f69a8ae
Jenkins CI <octgngame@gmail.com>
2014-11-16 05:44:06 +0000

    Auto generated commit for version 3.1.153.340-Test

--- octgnFX/o8build/Properties/AssemblyInfo.cs
+++ octgnFX/o8build/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.153.339")]
-[assembly: AssemblyFileVersion("3.1.153.3398")]
+[assembly: AssemblyVersion("3.1.153.340")]
+[assembly: AssemblyFileVersion("3.1.153.3408")]
