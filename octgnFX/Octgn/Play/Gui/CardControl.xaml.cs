db4e074d9443223962cc3e5bbcb4832469f99fcd
Kelly Elton <kelly.elton@skylabsonline.com>
2015-01-14 00:39:09 -0600

    Make anchor icon a bit different.

--- octgnFX/Octgn/Play/Gui/CardControl.xaml.cs
+++ octgnFX/Octgn/Play/Gui/CardControl.xaml.cs
@@ -111,7 +111,7 @@ namespace Octgn.Play.Gui
             if (markerSize == 0) markerSize = 20;
             markers.Margin = new Thickness(markerSize / 8);
             peekEyeIcon.Width = peekers.MinHeight = markerSize;
-            anchoredIcon.Width = markerSize;
+            anchoredIcon.Width = markerSize * 2;
             peekers.SetValue(TextBlock.FontSizeProperty, markerSize * 0.8);
             if (Program.GameEngine.Definition.CardCornerRadius > 0)
                 img.Clip = new RectangleGeometry();
