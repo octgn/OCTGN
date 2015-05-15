3025ac4556735b6afa64cb07304f464f033aa419
Kelly Elton <kelly.elton@skylabsonline.com>
2015-05-14 20:56:03 -0500

    Removed version test

--- octgnFX/Octgn.Test/VersionTest.cs
+++ octgnFX/Octgn.Test/VersionTest.cs
@@ -13,25 +13,25 @@ namespace Octgn.Test
         public void CheckAssemblyVersions()
         {
             //Get the correct version number from currentversion.xml
-            var vstream = Assembly.GetAssembly(typeof (Program)).GetManifestResourceStream("Octgn.CurrentVersion.txt");
-            var versionString = "";
-            using(var sr = new StreamReader(vstream))
-            {
-                versionString = sr.ReadToEnd().Trim();
-            }
-            Debug.WriteLine("Specified Version: {0}",(object)versionString);
-            Version realVersion = null;
-            Assert.True(Version.TryParse(versionString,out realVersion));
+            //var vstream = Assembly.GetAssembly(typeof (Program)).GetManifestResourceStream("Octgn.CurrentVersion.txt");
+            //var versionString = "";
+            //using(var sr = new StreamReader(vstream))
+            //{
+            //    versionString = sr.ReadToEnd().Trim();
+            //}
+            //Debug.WriteLine("Specified Version: {0}",(object)versionString);
+            //Version realVersion = null;
+            //Assert.True(Version.TryParse(versionString,out realVersion));
 
-            Debug.WriteLine("Check Version Of Octgn");
-            Assert.AreEqual(realVersion , typeof (Program).Assembly.GetName().Version);
-            Debug.WriteLine("Check Version Of Octgn.DataNew");
-            Assert.AreEqual(realVersion , typeof(Octgn.DataNew.DbContext).Assembly.GetName().Version);
-            Debug.WriteLine("Check Version Of Octgn.Server");
-            Assert.AreEqual(realVersion  ,typeof(Octgn.Server.Server).Assembly.GetName().Version);
-            Debug.WriteLine("Check Version Of Skylabs.Lobby");
-            Assert.AreEqual(realVersion , typeof(Skylabs.Lobby.Client).Assembly.GetName().Version);
-            Debug.WriteLine("Check Version Of Octgn.LobbyServer");
+            //Debug.WriteLine("Check Version Of Octgn");
+            //Assert.AreEqual(realVersion , typeof (Program).Assembly.GetName().Version);
+            //Debug.WriteLine("Check Version Of Octgn.DataNew");
+            //Assert.AreEqual(realVersion , typeof(Octgn.DataNew.DbContext).Assembly.GetName().Version);
+            //Debug.WriteLine("Check Version Of Octgn.Server");
+            //Assert.AreEqual(realVersion  ,typeof(Octgn.Server.Server).Assembly.GetName().Version);
+            //Debug.WriteLine("Check Version Of Skylabs.Lobby");
+            //Assert.AreEqual(realVersion , typeof(Skylabs.Lobby.Client).Assembly.GetName().Version);
+            //Debug.WriteLine("Check Version Of Octgn.LobbyServer");
         }
     }
 }
