ba4633ea15b13a419c7653c6ceb02dc27ec9c467
kellyelton <its.the.doc@gmail.com>
2015-05-15 02:09:25 -0500

    Updated version

--- Version.cs
+++ Version.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.1.12.377")]
-[assembly: AssemblyFileVersion("3.1.12.377")]
-[assembly: AssemblyInformationalVersion("3.1.12.377")]
+[assembly: AssemblyVersion("3.1.164.377")]
+[assembly: AssemblyFileVersion("3.1.164.377")]
+[assembly: AssemblyInformationalVersion("3.1.164.377")]
