5186f798917e3f403bbfc2b7550ff22867ea046f
Soul1355 <maiahee@gmail.com>
2015-05-30 17:31:16 -0700

    overlap drag handles instead
    also did styling on splitters a bit better

--- octgnFX/Octgn/Play/PlayWindow.xaml.cs
+++ octgnFX/Octgn/Play/PlayWindow.xaml.cs
@@ -1070,9 +1070,14 @@ namespace Octgn.Play
 
         private void ChatSplit_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            if (ChatGridChatPart.ActualHeight == ChatGridChatPart.MinHeight && e.VerticalChange >= 0)
+            if (ChatGridChatPart.ActualHeight <= ChatGridChatPart.MinHeight && e.VerticalChange >= 0) // + VerticalChange means shrinking chat box
             {
                 ChatGridChatPart.Height = new GridLength(0);
+                playerAreaGridSplitter.Margin = new Thickness(300,0,0,0);
+            }
+            else
+            {
+                playerAreaGridSplitter.Margin = new Thickness(0);
             }
         }
     }
