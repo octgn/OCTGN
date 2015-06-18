60976b21b92dc47d8711c2a3f0a4f5f2f5891377
AppVeyor Build <its.the.doc@gmail.com>
2015-06-18 15:55:04 +0000

    Auto generated commit for version 3.1.188.0 [skip ci]

--- Version.cs
+++ Version.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.1.187.0")]
-[assembly: AssemblyFileVersion("3.1.187.0")]
-[assembly: AssemblyInformationalVersion("3.1.187.0")]
+[assembly: AssemblyVersion("3.1.188.0")]
+[assembly: AssemblyFileVersion("3.1.188.0")]
+[assembly: AssemblyInformationalVersion("3.1.188.0")]
