264679face8fa7174e24033c3cf89211b8597ca0
Soul1355 <maiahee@gmail.com>
2015-07-07 16:31:41 -0700

    Improved performance of opaqueclickableimage

--- octgnFX/Octgn/Controls/OpaqueClickableImage.cs
+++ octgnFX/Octgn/Controls/OpaqueClickableImage.cs
@@ -14,7 +14,7 @@ namespace Octgn.Controls
     {
         protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
         {
-            var source = (BitmapSource)Source;
+            var source = new FormatConvertedBitmap((BitmapSource) Source, PixelFormats.Bgra32, null, 0);
 
             // Get the pixel of the source that was hit
             var x = (int)(hitTestParameters.HitPoint.X / ActualWidth * source.PixelWidth);
@@ -23,69 +23,15 @@ namespace Octgn.Controls
             if (x == source.PixelWidth) x--;
             if (y == source.PixelHeight) y--;
 
-            var pixel = GetPixels(source)[x, y];
+            var pixelxy = new Int32Rect(x, y, 1, 1);
+            var pixelbgra = new byte[4];
 
-            if (pixel.Alpha < 5)
+            source.CopyPixels(pixelxy, pixelbgra, source.PixelWidth * 4, 0);
+
+            if (pixelbgra[3] < 5)
                 return null;
 
             return new PointHitTestResult(this, hitTestParameters.HitPoint);
         }
-
-        public PixelColor[,] GetPixels(BitmapSource source)
-        {
-            if (source.Format != PixelFormats.Bgra32)
-                source = new FormatConvertedBitmap(source, PixelFormats.Bgra32, null, 0);
-
-            int width = source.PixelWidth;
-            int height = source.PixelHeight;
-            PixelColor[,] result = new PixelColor[width, height];
-
-            BitmapSourceHelper.CopyPixels(source, result, width * 4, 0);
-            //source.CopyPixels(result, width * 4, 0);
-            return result;
-        }
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PixelColor
-    {
-        public byte Blue;
-        public byte Green;
-        public byte Red;
-        public byte Alpha;
-    }
-
-    public static class BitmapSourceHelper
-    {
-#if UNSAFE
-  public unsafe static void CopyPixels(this BitmapSource source, PixelColor[,] pixels, int stride, int offset)
-  {
-    fixed(PixelColor* buffer = &pixels[0, 0])
-      source.CopyPixels(
-        new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight),
-        (IntPtr)(buffer + offset),
-        pixels.GetLength(0) * pixels.GetLength(1) * sizeof(PixelColor),
-        stride);
-  }
-#else
-        public static void CopyPixels(this BitmapSource source, PixelColor[,] pixels, int stride, int offset)
-        {
-            var height = source.PixelHeight;
-            var width = source.PixelWidth;
-            var pixelBytes = new byte[height * width * 4];
-            source.CopyPixels(pixelBytes, stride, 0);
-            int y0 = offset / width;
-            int x0 = offset - width * y0;
-            for (int y = 0; y < height; y++)
-                for (int x = 0; x < width; x++)
-                    pixels[x + x0, y + y0] = new PixelColor
-                    {
-                        Blue = pixelBytes[(y * width + x) * 4 + 0],
-                        Green = pixelBytes[(y * width + x) * 4 + 1],
-                        Red = pixelBytes[(y * width + x) * 4 + 2],
-                        Alpha = pixelBytes[(y * width + x) * 4 + 3],
-                    };
-        }
-#endif
     }
 }
