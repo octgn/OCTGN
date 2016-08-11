63c3292651b3a19255204021502956ef6a6b72a3
Kelly Elton <its.the.doc@gmail.com>
2016-08-11 14:36:04 -0400

    Undid MyGet hack fix since they updated their site

--- octgnFX/Octgn.Core/GameFeedManager.cs
+++ octgnFX/Octgn.Core/GameFeedManager.cs
@@ -2,24 +2,23 @@
 
 namespace Octgn.Core
 {
-	using System;
-	using System.Collections.Generic;
-	using System.IO;
-	using System.Linq;
-	using System.Net;
-	using System.Reflection;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Reflection;
 
-	using NuGet;
+    using NuGet;
 
-	using Octgn.Library;
-	using Octgn.Library.Exceptions;
-	using Octgn.Library.ExtensionMethods;
-	using Octgn.Library.Networking;
+    using Octgn.Library;
+    using Octgn.Library.Exceptions;
+    using Octgn.Library.ExtensionMethods;
+    using Octgn.Library.Networking;
 
-	using log4net;
-	using System.Xml;
+    using log4net;
 
-	public interface IGameFeedManager : IDisposable
+    public interface IGameFeedManager : IDisposable
     {
         event Action<String> OnUpdateMessage;
         void CheckForUpdates(bool localOnly = false, Action<int, int> onProgressUpdate = null);
@@ -288,17 +287,8 @@ namespace Octgn.Core
                     return new List<IPackage>();
                 }
                 Log.InfoFormat("Getting packages for feed {0}:{1}", url.Name, url.Url);
-                var ret = new List<DataServicePackage>();
-                var repo = PackageRepositoryFactory.Default.CreateRepository(url.Url);
-                ret = repo.GetPackages().ToList().Cast<DataServicePackage>().ToList();
-
-                foreach (var group in ret.GroupBy(x=>x.Id).AsParallel()) {
-					string title = null;
-                    foreach(var item in group) {
-						if (title == null) title = GetPackageTitle(item, url.Url);
-                        item.Title = title;
-                    }
-                }
+                var ret = new List<IPackage>();
+                ret = PackageRepositoryFactory.Default.CreateRepository(url.Url).GetPackages().ToList();
                 Log.InfoFormat("Finished getting packages for feed {0}:{1}", url.Name, url.Url);
                 return ret;
 
@@ -426,17 +416,7 @@ namespace Octgn.Core
             Log.Info("Exit");
         }
 
-		private string GetPackageTitle( IPackage package, string feedUrl ) {
-			//https://www.myget.org/F/octgngames/api/v2/Packages(Id='2f3dbb9b-67c4-41c9-b648-047a2fa4fc56',%20Version='1.0.0.0')/Title
-			feedUrl = feedUrl.TrimEnd('/');
-			var url = $"{feedUrl}/api/v2/Packages(Id='{package.Id}',%20Version='{package.Version}')/Title";
-
-			var doc = new XmlDocument();
-			doc.Load(url);
-			return doc.DocumentElement.InnerText;
-		}
-
-		public void Dispose()
+        public void Dispose()
         {
             Log.Info("Dispose called");
             OnUpdateFeedList = null;
