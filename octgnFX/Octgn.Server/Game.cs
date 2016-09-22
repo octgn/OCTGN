17bbb6a801f52e8434c688f66a096226ebc6c7d1
Kelly Elton <its.the.doc@gmail.com>
2016-09-21 22:45:46 -0400

    License

--- octgnFX/Octgn.Server/Game.cs
+++ octgnFX/Octgn.Server/Game.cs
@@ -1,4 +1,7 @@
-﻿using Octgn.Online.Library.Enums;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+using Octgn.Online.Library.Enums;
 using Octgn.Online.Library.Models;
 using System;
 using System.Collections.Generic;
