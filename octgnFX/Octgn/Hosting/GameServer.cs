c934c5ef31a228a6ec0ef2a007451a7e650cda84
Kelly Elton <its.the.doc@gmail.com>
2016-09-25 22:48:26 -0400

    LIcense

--- octgnFX/Octgn/Hosting/GameServer.cs
+++ octgnFX/Octgn/Hosting/GameServer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin.Hosting;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+using Microsoft.Owin.Hosting;
 using System;
 using Owin;
 using Microsoft.Owin.Cors;
