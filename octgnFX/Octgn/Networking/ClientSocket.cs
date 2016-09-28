377ff23adea31f44880bfa0caed4d8707ec015d1
Kelly Elton <its.the.doc@gmail.com>
2016-09-27 22:25:53 -0400

    Client api's should be good.

--- octgnFX/Octgn/Networking/ClientSocket.cs
+++ octgnFX/Octgn/Networking/ClientSocket.cs
@@ -2,86 +2,87 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 using System;
-using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 using Octgn.Library.Networking;
+using Microsoft.AspNet.SignalR.Client;
+using Octgn.Server.Signalr;
 
 namespace Octgn.Networking
 {
-    public class ClientSocket : ReconnectingSocketBase
+    public class ClientSocket : IDisposable
     {
-        internal IServerCalls Rpc { get; set; }
+		internal static ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        internal HubRpc Rpc { get; set; }
         internal Handler Handler { get; set; }
 
-        public ulong Muted { get; set; }
+        private readonly HubConnection _connection;
+        private readonly IHubProxy _hub;
 
-        public ClientSocket(IPAddress address, int port)
-            : base(0, TimeSpan.Zero, LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType))
-        {
-            this.Setup(new IPEndPoint(address, port), new ClientMessageProcessor());
-            this.Client.Client.SendTimeout = 4000;
+        public ulong Muted { get; set; }
+        public IPEndPoint EndPoint { get; private set; }
+
+        public ClientSocket(IPAddress address, int port) {
+            EndPoint = new IPEndPoint(address, port);
+            var path = $"http://{address}:{port}/";
+            _connection = new HubConnection(path);
+            _hub = _connection.CreateHubProxy(nameof(GameHub));
+
+            _connection.StateChanged += Connection_StateChanged;
+            _connection.Reconnected += Connection_Reconnected;
+
             Handler = new Handler();
-            Rpc = new BinarySenderStub(this);
+            Rpc = new HubRpc();
         }
 
-        public override void OnConnectionEvent(object sender, SocketConnectionEvent e)
-        {
-            base.OnConnectionEvent(sender, e);
-            switch (e)
-            {
-                case SocketConnectionEvent.Disconnected:
+        public async Task Connect() => await _connection.Start();
+
+        internal void ForceDisconnect() => _connection.Stop();
+
+        protected void SetupHandler() {
+            Handler.InitializeHub(_hub);
+            Rpc.InitializeHub(_hub);
+        }
+
+        private void Connection_StateChanged(StateChange obj) {
+            switch (obj.NewState) {
+                case ConnectionState.Reconnecting:
+                case ConnectionState.Connecting:
+                    break;
+                case ConnectionState.Disconnected:
                     if (Program.GameEngine != null)
                         Program.GameEngine.IsConnected = false;
                     Program.GameMess.Warning("You have been disconnected from server.");
                     break;
-                case SocketConnectionEvent.Connected:
+                case ConnectionState.Connected:
                     if (Program.GameEngine != null)
                         Program.GameEngine.IsConnected = true;
+                    SetupHandler();
                     break;
-                case SocketConnectionEvent.Reconnected:
-                    if (Program.GameEngine != null)
-                    {
-                        Program.GameEngine.IsConnected = true;
-                        Program.GameEngine.Resume();
-                    }
-                    Program.GameMess.System("You have reconnected");
-
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException("e");
+                default: throw new ArgumentOutOfRangeException(nameof(obj));
             }
         }
 
-        public override void OnDataReceived(object sender, byte[] data)
-        {
-            Program.Dispatcher.BeginInvoke(new Action(() =>
+        private void Connection_Reconnected() {
+            if (Program.GameEngine != null)
             {
-                Handler.ReceiveMessage(data.Skip(4).ToArray());
-            }));
+                Program.GameEngine.IsConnected = true;
+                Program.GameEngine.Resume();
+            }
+            Program.GameMess.System("You have reconnected");
         }
 
         public void SeverConnectionAtTheKnee()
         {
-            this.Client.Close();
+            this._connection.Stop();
         }
 
-        public void StartPings()
-        {
-            Log.Debug("StartPings");
-            Task.Factory.StartNew(
-                () =>
-                {
-                    while (Status == SocketStatus.Connected)
-                    {
-                        Rpc.Ping();
-                        Thread.Sleep(2000);
-                    }
-                    Log.Debug("StartPings Done Pinging");
-                });
+        public void Dispose() {
+            _connection.StateChanged -= Connection_StateChanged;
+            _connection.Reconnected -= Connection_Reconnected;
+            _connection.Dispose();
         }
     }
 
