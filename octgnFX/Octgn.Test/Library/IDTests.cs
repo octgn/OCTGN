6cb54ba9432b783811454a94ccc3028fc859772c
Kelly Elton <its.the.doc@gmail.com>
2016-10-02 23:26:30 -0400

    uint is 32 bit not 16...

---
+++ octgnFX/Octgn.Test/Library/IDTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Octgn.Test.Library
+{
+    public class IDTests
+    {
+        [Test]
+        public void ID_ConvertsUniformly() {
+            var id = new ID(IDType.Group, 12, 24);
+            ulong uid = id;
+            ID id2 = uid;
+            ulong uid2 = id2;
+
+            Assert.AreEqual(id.Id, id2.Id);
+            Assert.AreEqual(id.GameId, id2.GameId);
+            Assert.AreEqual(id.PlayerId, id2.PlayerId);
+            Assert.AreEqual(id.Type, id2.Type);
+            Assert.AreEqual(uid, uid2);
+        }
+    }
+}
