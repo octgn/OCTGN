f4cdf5296b89481ba637b5978ccb3758f86d2b79
Kelly Elton <its.the.doc@gmail.com>
2016-10-03 22:41:24 -0400

    yup

--- octgnFX/Octgn.Library/IDHelper.cs
+++ octgnFX/Octgn.Library/IDHelper.cs
@@ -5,11 +5,13 @@ namespace Octgn
     public class IDHelper
     {
         public static Guid GlobalPlayerId { get; }
+        public static Guid TableId { get; }
 
         public static Guid NewId() => Guid.NewGuid();
 
         static IDHelper() {
             GlobalPlayerId = new Guid((uint)0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            TableId = Guid.Parse("61d1644d-224d-453b-967e-9497445c661e");
         }
     }
 }
