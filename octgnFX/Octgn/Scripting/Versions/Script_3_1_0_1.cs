dd564563f9c5205f2cf3839ab355f152535d8d44
brine <spreetarded@gmail.com>
2016-10-10 15:51:19 -0400

    consolidated some of the card move stuff

--- octgnFX/Octgn/Scripting/Versions/Script_3_1_0_1.cs
+++ octgnFX/Octgn/Scripting/Versions/Script_3_1_0_1.cs
@@ -604,18 +604,8 @@ namespace Octgn.Scripting.Versions
             if (card.Group != Program.GameEngine.Table && card.Group.Controller != Player.LocalPlayer)
                 Program.GameMess.Warning(String.Format("{0} Can't set index of {1} in {2} because they don't control it.", Player.LocalPlayer.Name, card, card.Group));
 
-            if (TableOnly)
+            if (!TableOnly || (TableOnly && card.Group is Table))
             {
-                if (card.Group is Table)
-                    QueueAction(
-                        () =>
-                        {
-                            //Program.GameEngine.EventProxy.MuteEvents = true;
-                            card.MoveToTable((int)card.X, (int)card.Y, card.FaceUp, idx, true);
-                            //Program.GameEngine.EventProxy.MuteEvents = false;
-                        });
-            }
-            else
                 QueueAction(
                     () =>
                     {
@@ -623,6 +613,7 @@ namespace Octgn.Scripting.Versions
                         card.MoveToTable((int)card.X, (int)card.Y, card.FaceUp, idx, true);
                         //Program.GameEngine.EventProxy.MuteEvents = false;
                     });
+            }
         }
 
         public void CardTarget(int id, bool active)
