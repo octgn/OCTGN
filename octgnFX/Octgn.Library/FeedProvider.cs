f55d9e1e83cce5416232728142dac8fe1ff541ae
Kelly Elton <its.the.doc@gmail.com>
2017-01-02 19:19:41 -0500

    Fixed adding feeds being broken

--- octgnFX/Octgn.Library/FeedProvider.cs
+++ octgnFX/Octgn.Library/FeedProvider.cs
@@ -2,6 +2,7 @@
 //  * License, v. 2.0. If a copy of the MPL was not distributed with this
 //  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,32 +17,31 @@ namespace Octgn.Library
 {
     public interface IFeedProvider
     {
-        IEnumerable<NamedUrl> Feeds { get; }
+        IEnumerable<NamedUrl> AllFeeds { get; }
+        IEnumerable<NamedUrl> UserFeeds { get; }
         IEnumerable<NamedUrl> LocalFeeds { get; }
+        IEnumerable<NamedUrl> ReservedFeeds { get; }
 
-        void AddFeed(NamedUrl feed);
+        void AddFeed( NamedUrl feed );
 
-        void RemoveFeed(NamedUrl feed);
+        void RemoveFeed( NamedUrl feed );
     }
 
     public class FeedProvider : IFeedProvider
     {
+        private static ILog Log = LogManager.GetLogger( MethodBase.GetCurrentMethod().DeclaringType );
+
         #region Singleton
 
         internal static IFeedProvider SingletonContext { get; set; }
 
         private static readonly object FeedProviderSingletonLocker = new object();
 
-        public static IFeedProvider Instance
-        {
-            get
-            {
-                if (SingletonContext == null)
-                {
-                    lock (FeedProviderSingletonLocker)
-                    {
-                        if (SingletonContext == null)
-                        {
+        public static IFeedProvider Instance {
+            get {
+                if( SingletonContext == null ) {
+                    lock( FeedProviderSingletonLocker ) {
+                        if( SingletonContext == null ) {
                             SingletonContext = new FeedProvider();
                         }
                     }
@@ -52,154 +52,121 @@ namespace Octgn.Library
 
         #endregion Singleton
 
-        internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public IEnumerable<NamedUrl> AllFeeds => ReservedFeeds.Concat( UserFeeds );
 
-        private DateTime nextCheck;
+        public IEnumerable<NamedUrl> UserFeeds => GetUserFeeds();
 
-        private List<NamedUrl> feeds;
+        public IEnumerable<NamedUrl> LocalFeeds => _reservedFeeds.Where( x => x.Name == "Local" ).ToArray();
 
-        public IEnumerable<NamedUrl> Feeds => GetFeeds();
+        public IEnumerable<NamedUrl> ReservedFeeds => _reservedFeeds;
 
-        public IEnumerable<NamedUrl> LocalFeeds => GetFeeds( true );
+        private DateTime cachedFeedsExpireDate;
+        private NamedUrl[] cachedFeeds;
 
-        public IEnumerable<NamedUrl> ReservedFeeds => GetFeeds( reservedOnly: true );
+        private readonly NamedUrl[] _reservedFeeds;
+        private readonly object _feedLock = new object();
 
-        internal FeedProvider()
-        {
-            nextCheck = DateTime.MinValue;
-            feeds = new List<NamedUrl>();
+        internal FeedProvider() {
+            cachedFeedsExpireDate = DateTime.MinValue;
+            cachedFeeds = new NamedUrl[0];
+            _reservedFeeds = new NamedUrl[] {
+                new NamedUrl("Local", Config.Instance.Paths.LocalFeedPath, null, null),
+                new NamedUrl("OCTGN Official", Config.Instance.Paths.MainOctgnFeed, null, null),
+                new NamedUrl("The Spoils", Config.Instance.Paths.SpoilsFeedPath, null, null),
+            };
         }
 
-        private IEnumerable<NamedUrl> GetFeeds(bool localOnly = false, bool reservedOnly = false)
-        {
-            if (DateTime.Now > nextCheck)
-            {
-                var ret = new List<NamedUrl>();
-                ret.Add(new NamedUrl("Local", Config.Instance.Paths.LocalFeedPath, null, null));
-
-                ret.Add(new NamedUrl("OCTGN Official", Config.Instance.Paths.MainOctgnFeed, null, null));
-                ret.Add(new NamedUrl("The Spoils", Config.Instance.Paths.SpoilsFeedPath, null, null));
-                if(!reservedOnly) ret.AddRange(this.GetFeedsList().ToList());
-
-                feeds = ret;
-                nextCheck = DateTime.Now.AddMinutes(1);
-            }
-            if (!localOnly) return feeds.ToArray();
-            return this.feeds.Where(x => x.Name == "Local").ToArray();
-        }
-
-        public void AddFeed(NamedUrl feed)
-        {
-            lock (this)
-            {
+        public void AddFeed( NamedUrl feed ) {
+            lock( _feedLock ) {
                 if( ReservedFeeds.Any( x => x.Name.Equals( feed.Name, StringComparison.InvariantCultureIgnoreCase ) ) )
                     return;
-                //throw new UserMessageException("You can not replace built in feeds");
-                var remList = feeds.Where(x => x.Name.Equals(feed.Name)).ToArray();
-                foreach (var r in remList)
-                {
-                    feeds.Remove(r);
-                }
-                feeds.Add(feed);
-                this.WriteFeedList();
+                List<NamedUrl> newList = UserFeeds.Where( x => !x.Name.Equals( feed.Name ) )
+                    .ToList();
+
+                newList.Add( feed );
+                WriteFeedListToDisk( newList );
+
+                cachedFeedsExpireDate = DateTime.MinValue; // Expire cache
             }
         }
 
-        public void RemoveFeed(NamedUrl feed)
-        {
-            lock (this)
-            {
+        public void RemoveFeed( NamedUrl feed ) {
+            lock( _feedLock ) {
                 if( ReservedFeeds.Any( x => x.Name.Equals( feed.Name, StringComparison.InvariantCultureIgnoreCase ) ) )
                     return;
-                //throw new UserMessageException("Can not remove built in feeds.");
-                var remList = feeds.Where(x => x.Name.Equals(feed.Name)).ToArray();
-                foreach (var r in remList)
-                {
-                    feeds.Remove(r);
-                }
-                this.WriteFeedList();
+                NamedUrl[] remList = UserFeeds.Where( x => !x.Name.Equals( feed.Name ) ).ToArray();
+                WriteFeedListToDisk( remList );
+
+                cachedFeedsExpireDate = DateTime.MinValue; // Expire cache
             }
         }
 
-        public List<NamedUrl> RemoveLocalFeeds(List<NamedUrl> feedList)
-        {
-            feedList = (from line in feedList
-                        where ReservedFeeds.All( x => !x.Name.Equals( line.Name, StringComparison.InvariantCultureIgnoreCase ) )
-                        select line).ToList();
-            return feedList;
-        }
-
-        internal IEnumerable<NamedUrl> GetFeedsList()
-        {
+        internal static IEnumerable<NamedUrl> ReadFeedListFromDisk() {
             Stream stream = null;
-            var wasLocked = false;
-            while (!X.Instance.File.OpenFile(Config.Instance.Paths.FeedListPath, FileMode.OpenOrCreate, FileShare.None, TimeSpan.FromDays(1), out stream))
-            {
+            bool wasLocked = false;
+            while( !X.Instance.File.OpenFile( Config.Instance.Paths.FeedListPath, FileMode.OpenOrCreate, FileShare.None, TimeSpan.FromDays( 1 ), out stream ) ) {
                 wasLocked = true;
-                Log.Info("Getting feed list file still locked.");
-                Thread.Sleep(2000);
+                Log.Info( "Getting feed list file still locked." );
+                Thread.Sleep( 2000 );
             }
-            if (wasLocked) Log.Debug("Getting feed list file unlocked.");
-            using (var sr = new StreamReader(stream))
-            {
-                var lines = sr.ReadToEnd()
-                    .Replace("\r", "")
-                    .Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Where(x => !String.IsNullOrWhiteSpace(x.Trim()))
-                    .Select(x => x.Split(new[] { (char)1 }, StringSplitOptions.RemoveEmptyEntries))
-                    .Select(x =>
-                    {
-                        if (x.Length != 2 && x.Length != 4)
+            if( wasLocked ) Log.Debug( "Getting feed list file unlocked." );
+            using( StreamReader sr = new StreamReader( stream ) ) {
+                List<NamedUrl> lines = sr.ReadToEnd()
+                    .Replace( "\r", "" )
+                    .Split( new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries )
+                    .Where( x => !String.IsNullOrWhiteSpace( x.Trim() ) )
+                    .Select( x => x.Split( new[] { (char)1 }, StringSplitOptions.RemoveEmptyEntries ) )
+                    .Select( x => {
+                        if( x.Length != 2 && x.Length != 4 )
                             return null;
-                        if (x.Length == 2)
-                        {
-                            return new NamedUrl(x[0].Trim(), x[1].Trim(), null, null);
+                        if( x.Length == 2 ) {
+                            return new NamedUrl( x[0].Trim(), x[1].Trim(), null, null );
                         }
-                        if (x.Length == 4)
-                        {
-                            return new NamedUrl(x[0].Trim(), x[1].Trim(), x[2].Trim(), x[3].Trim());
+                        if( x.Length == 4 ) {
+                            return new NamedUrl( x[0].Trim(), x[1].Trim(), x[2].Trim(), x[3].Trim() );
                         }
                         return null;
-                    })
-                    .Where(x => x != null).ToList();
+                    } )
+                    .Where( x => x != null ).ToList();
 
-                lines.ForEach(line => line.Url = CorrectMyGetFeed(line.Url));
-                // Don't read local and octgn official from file
-                lines = this.RemoveLocalFeeds(lines);
+                lines.ForEach( line => line.Url = CorrectMyGetFeed( line.Url ) );
 
                 return lines;
             }
         }
 
-        internal void WriteFeedList()
-        {
-            var lines = Feeds.ToList();
+        internal static void WriteFeedListToDisk( IEnumerable<NamedUrl> feeds ) {
+            List<NamedUrl> lines = feeds.ToList();
 
             // correct myGet URLS -- correct them both here before the check to make sure we don't get an http and https version of the same.
-            lines.ForEach(line => line.Url = CorrectMyGetFeed(line.Url));
-
-            // Don't write local and octgn official to file
-            lines = this.RemoveLocalFeeds(lines);
+            lines.ForEach( line => line.Url = CorrectMyGetFeed( line.Url ) );
 
             Stream stream = null;
-            while (!X.Instance.File.OpenFile(Config.Instance.Paths.FeedListPath, FileMode.Create, FileShare.None, TimeSpan.FromDays(1), out stream))
-            {
-                Thread.Sleep(10);
+            while( !X.Instance.File.OpenFile( Config.Instance.Paths.FeedListPath, FileMode.Create, FileShare.None, TimeSpan.FromDays( 1 ), out stream ) ) {
+                Thread.Sleep( 10 );
             }
-            using (var sr = new StreamWriter(stream))
-            {
-                lines.ForEach(line => sr.WriteLine(line.Name + (char)1 + line.Url + (char)1 + line.Username + (char)1 + line.Password));
+            using( StreamWriter sr = new StreamWriter( stream ) ) {
+                lines.ForEach( line => sr.WriteLine( line.Name + (char)1 + line.Url + (char)1 + line.Username + (char)1 + line.Password ) );
             }
         }
 
-        private string CorrectMyGetFeed(string url)
-        {
-            var bad = @"http://www.myget.org";
-            var good = @"https://www.myget.org";
+        private IEnumerable<NamedUrl> GetUserFeeds() {
+            lock( _feedLock ) {
+                if( DateTime.Now > cachedFeedsExpireDate ) {
+                    cachedFeeds = ReadFeedListFromDisk().ToArray();
+                    cachedFeedsExpireDate = DateTime.Now.AddMinutes( 1 );
+                }
 
-            if (url.ToLower().StartsWith(bad))
-            {
-                var remainder = url.Substring(bad.Length);
+                return cachedFeeds.ToArray();
+            }
+        }
+
+        private static string CorrectMyGetFeed( string url ) {
+            string bad = @"http://www.myget.org";
+            string good = @"https://www.myget.org";
+
+            if( url.ToLower().StartsWith( bad ) ) {
+                string remainder = url.Substring( bad.Length );
                 url = good + remainder;
             }
 
