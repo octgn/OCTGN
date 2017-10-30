3b66435866cb226c28aac0ec3e77628917e2048a
AppVeyor Build <its.the.doc@gmail.com>
2017-10-29 20:17:54 -0700

    Auto generated commit for version 3.1.302.0 [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+#3.1.302.0
+
 #3.1.301.0
 
 #3.1.300.0
@@ -3428,6 +3430,7 @@ To come...There are 30 ish more changes that can be found in our commit list htt
 
 + Added me.isActivePlayer
 + Added openUrl(url)
+
 
 
 
--- Version.cs
+++ Version.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.1.301.0")]
-[assembly: AssemblyFileVersion("3.1.301.0")]
-[assembly: AssemblyInformationalVersion("3.1.301.0")]
+[assembly: AssemblyVersion("3.1.302.0")]
+[assembly: AssemblyFileVersion("3.1.302.0")]
+[assembly: AssemblyInformationalVersion("3.1.302.0")]
