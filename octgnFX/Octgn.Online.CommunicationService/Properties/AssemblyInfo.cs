4acda4a1329f937a2e99dd1b2f9f8b4cbbdbcd63
Kelly Elton <its.the.doc@gmail.com>
2017-10-30 13:53:06 -0500

    Installed some missing nuget packages Updated port in GameService to 5223 Turned on log4net in communication service

--- octgnFX/Octgn.Online.CommunicationService/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Online.CommunicationService/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1bc6fed6-2f2a-4ed5-a3db-67dd2070b292")]
