de8b3af0746dff9893a562ed058b652c6bc47496
AppVeyor Build <its.the.doc@gmail.com>
2018-02-03 19:50:04 +0000

    Auto generated commit for version 3.2.62.0 [skip ci]

--- Version.cs
+++ Version.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.2.61.0")]
-[assembly: AssemblyFileVersion("3.2.61.0")]
-[assembly: AssemblyInformationalVersion("3.2.61.0")]
+[assembly: AssemblyVersion("3.2.62.0")]
+[assembly: AssemblyFileVersion("3.2.62.0")]
+[assembly: AssemblyInformationalVersion("3.2.62.0")]
