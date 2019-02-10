d8ace5f6004b36be6b6e490afc9feba08c936898
Andrew Rusling <andrewrusling@hotmail.com>
2019-02-10 20:27:49 +1000

    Ready to create pull request

---
+++ octgnFX/Octgn.Core/Plugin/IEventBindingDeckBuilderPlugin.cs
@@ -0,0 +1,15 @@
+﻿using Octgn.Core.DataManagers;
+
+namespace Octgn.Core.Plugin
+{
+    public interface IEventBindingDeckBuilderPlugin : IDeckBuilderPlugin
+    {
+        /// <summary>
+        /// Overload the OnLoad from IDeckBuilderPlugin also taking in the calling object, so that we can bind to its events.
+        /// </summary>
+        /// <param name="caller">Octgn.DeckBuilder.DeckBuilderWindow</param>
+        /// <param name="gameManager"></param>
+        void OnLoad(object caller, GameManager gameManager);
+    }
+
+}
