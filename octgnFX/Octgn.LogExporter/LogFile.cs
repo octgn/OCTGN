e335312f019cad9ff6ed20f9289174ab0935871a
Andrew Rusling <andrewrusling@hotmail.com>
2019-02-12 08:41:44 +1000

    Revert "Revert "Created LogExporter""
    This reverts commit 7bf2dcc24a649796c4d6aa2ca58b536a26f2c919.

---
+++ octgnFX/Octgn.LogExporter/LogFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Octgn.LogExporter
+{
+    public class LogFile
+    {
+        private readonly string _path;
+
+        public LogFile(string directory) {
+            _path = Path.Combine(directory, "Export.log");
+
+            if (File.Exists(_path))
+                throw new InvalidOperationException($"File {_path} already exists.");
+        }
+
+        public void Info(string message) {
+            WriteLine("INFO ", message);
+        }
+
+        public void Error(string message) {
+            WriteLine("ERROR", message);
+        }
+        
+        public void Error(string message, Exception ex) {
+            WriteLine("ERROR", message + Environment.NewLine + ex.ToString());
+        }
+
+        private void WriteLine(string eventType, string line) {
+            using (var logFileStream = File.Open(_path, FileMode.Append, FileAccess.Write, FileShare.Read))
+            using (var logStreamWriter = new StreamWriter(logFileStream)) {
+                logStreamWriter.WriteLine(eventType + ": " + DateTime.Now.ToString("u") + ": " + line);
+            }
+        }
+    }
+}
