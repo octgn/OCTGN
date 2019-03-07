94b2110f94c235d320750089c2030ed2f5b65100
Kelly Elton <its.the.doc@gmail.com>
2019-03-07 00:10:49 -0600

    Don't categorize deck stats

--- octgnFX/Octgn/Play/Gui/DeckStats.xaml.cs
+++ octgnFX/Octgn/Play/Gui/DeckStats.xaml.cs
@@ -30,48 +30,27 @@ namespace Octgn.Play.Gui
         }
         private bool _isVisible;
 
-        public void Clear() {
-            foreach(var group in Groups) {
-                group.Clear();
-            }
-        }
-
-        public ObservableCollection<DeckStatsGroupViewModel> Groups { get; }
-
-        public DeckStatsViewModel() {
-            Groups = new ObservableCollection<DeckStatsGroupViewModel>();
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName) {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class DeckStatsGroupViewModel : INotifyPropertyChanged
-    {
-        public string Name {
-            get => _name;
-            set {
-                if (value == _name) return;
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
-        private string _name;
-
         public ObservableCollection<DeckStatsCardViewModel> Cards { get; }
 
-        public DeckStatsGroupViewModel(Group group) {
-            Name = group.Name;
+        public DeckStatsViewModel() {
             Cards = new ObservableCollection<DeckStatsCardViewModel>();
-
-            group.Cards.CollectionChanged += Cards_CollectionChanged;
         }
 
-        public void Clear() {
-            Cards.Clear();
+        public void Reset() {
+            foreach(var card in Cards.ToArray()) {
+                card.Reset();
+                Cards.Remove(card);
+            }
+        }
+
+        public void AddCard(Card card) {
+            var cardVm = Cards.FirstOrDefault(x => x.ModelId == card.Type.Model.Id);
+            if (cardVm == null) {
+                cardVm = new DeckStatsCardViewModel(card);
+                AddCardVm(cardVm);
+            }
+
+            cardVm.AddCard(card);
         }
 
         private void AddCardVm(DeckStatsCardViewModel vm) {
@@ -100,57 +79,6 @@ namespace Octgn.Play.Gui
             }
         }
 
-        private void Cards_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
-            switch (e.Action) {
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
-                    foreach (var addedCard in e.NewItems.Cast<Card>()) {
-                        var cardVm = Cards.FirstOrDefault(x => x.ModelId == addedCard.Type.Model.Id);
-                        if(cardVm == null) {
-                            cardVm = new DeckStatsCardViewModel(addedCard);
-                            AddCardVm(cardVm);
-                        }
-
-                        cardVm.AddCard(addedCard);
-                    }
-                    break;
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
-                    foreach(var removedCard in e.OldItems.Cast<Card>()) {
-                        var cardVm = Cards.FirstOrDefault(x => x.ModelId == removedCard.Type.Model.Id);
-                        cardVm.RemoveCard(removedCard);
-
-                        if(cardVm.CardCount == 0) {
-                            Cards.Remove(cardVm);
-                        }
-                    }
-                    break;
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Replace:
-                    for(var i = 0; i < e.OldItems.Count; i++) {
-                        var oldCard = (Card)e.OldItems[i];
-                        var newCard = (Card)e.NewItems[i];
-
-                        var cardVm = Cards.FirstOrDefault(x => x.ModelId == oldCard.Type.Model.Id);
-                        cardVm.RemoveCard(oldCard);
-                        if (cardVm.CardCount == 0)
-                            Cards.Remove(cardVm);
-
-                        var newVm = Cards.FirstOrDefault(x => x.ModelId == newCard.Type.Model.Id);
-                        if (newVm == null) {
-                            newVm = new DeckStatsCardViewModel(newCard);
-                            AddCardVm(cardVm);
-                        }
-                        newVm.AddCard(newCard);
-                    }
-                    break;
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Move:
-                    // Don't care, the order is unimportant
-                    break;
-                case System.Collections.Specialized.NotifyCollectionChangedAction.Reset:
-                    Cards.Clear();
-                    break;
-                default: throw new InvalidOperationException($"Can't handle {e.Action}");
-            }
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName) {
@@ -165,12 +93,10 @@ namespace Octgn.Play.Gui
         public string Name { get; }
 
         public int CardCount {
-            get => _ids.Count;
+            get => _cards.Count(x => !x.FaceUp);
         }
 
-        public IEnumerable<int> Ids => _ids.AsEnumerable();
-
-        private readonly HashSet<int> _ids = new HashSet<int>();
+        private readonly HashSet<Card> _cards = new HashSet<Card>();
 
         public BitmapImage Image {
             get => _image;
@@ -209,22 +135,24 @@ namespace Octgn.Play.Gui
             ImageUtils.GetCardImage(card.Type.Model, x => Image = x, false);
         }
 
-        public void ResetCounters() {
-            _ids.Clear();
-
-            OnPropertyChanged(nameof(CardCount));
-        }
-
         public void AddCard(Card card) {
-            _ids.Add(card.Id);
+            _cards.Add(card);
+            card.PropertyChanged += Card_PropertyChanged;
             OnPropertyChanged(nameof(CardCount));
-            Flash();
         }
 
-        public void RemoveCard(Card card) {
-            _ids.Remove(card.Id);
-            OnPropertyChanged(nameof(CardCount));
-            Flash();
+        public void Reset() {
+            foreach (var card in _cards) {
+                card.PropertyChanged -= Card_PropertyChanged;
+            }
+            _cards.Clear();
+        }
+
+        private void Card_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+            if (e.PropertyName == nameof(Card.FaceUp)) {
+                OnPropertyChanged(nameof(CardCount));
+                Flash();
+            }
         }
 
         private async void Flash() {
