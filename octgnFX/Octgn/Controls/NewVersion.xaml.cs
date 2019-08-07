3a471b915638005dc9e8b97c9d58513bf840555b
Kelly Elton <its.the.doc@gmail.com>
2019-08-06 20:50:24 -0500

    Add NewVersion bar for 3.4

---
+++ octgnFX/Octgn/Controls/NewVersion.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Octgn.Controls
+{
+    public partial class NewVersionBar
+    {
+        public NewVersionBar() {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            Program.LaunchUrl(AppConfig.WebsitePath);
+        }
+    }
+}
