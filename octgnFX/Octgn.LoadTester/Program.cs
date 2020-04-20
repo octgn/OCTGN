9ccffc0c0919948286adb437f72ac038d2a043b9
Kelly Elton <its.the.doc@gmail.com>
2020-04-19 20:11:26 -0500

    Room spammer

---
+++ octgnFX/Octgn.LoadTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Octgn.LoadTester
+{
+    class Program
+    {
+        static async Task Main(string[] args) {
+            using (var cts = new CancellationTokenSource()) {
+                try {
+                    Console.CancelKeyPress += (_, __) => {
+                        try {
+                            cts.Cancel();
+                        } catch (ObjectDisposedException) { }
+                    };
+
+                    var tester = new Tester();
+
+                    await tester.Run(cts.Token);
+                } catch (Exception ex) {
+                    Console.Error.WriteLine(ex);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("== DONE ==");
+        }
+    }
+}
---
+++ octgnFX/Octgn.LoadTester/Tester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Octgn.LoadTester
+{
+    public class Tester
+    {
+        public Tester() {
+
+        }
+
+        public Task Run(CancellationToken cancellationToken) {
+            var nodes = new List<Node>();
+
+            var nodeTasks = new List<Task>();
+
+            for (var i = 0; i < 10; i++) {
+                var node = new Node();
+
+                var nodeTask = node.Run(cancellationToken);
+
+                nodes.Add(node);
+
+                nodeTasks.Add(nodeTask);
+            }
+
+            return Task.WhenAll(nodeTasks);
+        }
+    }
+}
