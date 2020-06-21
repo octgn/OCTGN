2789a5a166fee3ae0d93af087ed01548814905f3
Kelly Elton <its.the.doc@gmail.com>
2020-06-20 22:12:52 -0500

    meh

---
+++ octgnFX/Octgn.Sdk/IMainMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Octgn.Sdk
+{
+    public interface IMainMenuPlugin
+    {
+        string Background { get; }
+
+        string MenuBackground { get; }
+
+        string ButtonBackground { get; }
+
+        string ButtonBackgroundHover { get; }
+
+        string ButtonBorder { get; }
+
+        string ButtonFont { get; }
+
+        IEnumerable<IMenuItem> MenuItems { get; }
+
+    }
+
+    public interface IMenuItem
+    {
+        string Text { get; }
+
+        Task OnClick();
+    }
+
+    public interface IPlugin
+    {
+        Task OnStart(OctgnApp app);
+    }
+
+    public class DefaultJodsMainMenuPlugin : IPlugin
+    {
+        public Task OnStart(OctgnApp app) {
+            app.MainMenus.Add(new DefaultJodsMainMenu());
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DefaultJodsMainMenu : IMainMenuPlugin
+    {
+        public string Background => throw new NotImplementedException();
+
+        public string MenuBackground => throw new NotImplementedException();
+
+        public string ButtonBackground => throw new NotImplementedException();
+
+        public string ButtonBackgroundHover => throw new NotImplementedException();
+
+        public string ButtonBorder => throw new NotImplementedException();
+
+        public string ButtonFont => throw new NotImplementedException();
+
+        public IEnumerable<IMenuItem> MenuItems => throw new NotImplementedException();
+    }
+}
---
+++ octgnFX/Octgn.Sdk/OctgnApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Octgn.Sdk
+{
+    public class OctgnApp
+    {
+        public ObservableCollection<IMainMenuPlugin> MainMenus { get; } = new ObservableCollection<IMainMenuPlugin>();
+
+
+    }
+}
