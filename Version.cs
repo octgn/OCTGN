82dc8b6f1a6155121257cd0576191609508838a9
AppVeyor Build <its.the.doc@gmail.com>
2020-08-14 06:25:09 +0000

    Auto generated commit for version 3.4.304.0 [skip ci]

--- Version.cs
+++ Version.cs
@@ -4,6 +4,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("3.4.301.0")]
-[assembly: AssemblyFileVersion("3.4.301.0")]
-[assembly: AssemblyInformationalVersion("3.4.301.0")]
+[assembly: AssemblyVersion("3.4.304.0")]
+[assembly: AssemblyFileVersion("3.4.304.0")]
+[assembly: AssemblyInformationalVersion("3.4.304.0")]
