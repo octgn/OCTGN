4c4714db829cfc0b8376483343294989ce1837fa
Kelly Elton <its.the.doc@gmail.com>
2020-08-15 16:39:35 -0500

    Don't crash if no discord

--- octgnFX/Octgn.JodsEngine/Loaders/DiscordLoader.cs
+++ octgnFX/Octgn.JodsEngine/Loaders/DiscordLoader.cs
@@ -10,25 +10,24 @@ namespace Octgn.Loaders
         private readonly log4net.ILog Log
             = log4net.LogManager.GetLogger(typeof(DiscordLoader));
 
-        public DiscordLoader()
-        {
-
+        public DiscordLoader() {
         }
 
         public string Name => "Discord";
 
-        public Task Load(ILoadingView view)
-        {
-            return Task.Run(() =>
-            {
-                Program.Discord = new DiscordWrapper();
+        public Task Load(ILoadingView view) {
+            return Task.Run(() => {
+                try {
+                    Program.Discord = new DiscordWrapper();
 
-                Program.Discord.Error += Discord_Error;
+                    Program.Discord.Error += Discord_Error;
+                } catch (Exception ex) {
+                    Log.Warn($"Discord Creation Error: {ex.Message}", ex);
+                }
             });
         }
 
-        private void Discord_Error(object sender, Exception e)
-        {
+        private void Discord_Error(object sender, Exception e) {
             Log.Warn($"Discord Error: {e.Message}", e);
         }
     }
