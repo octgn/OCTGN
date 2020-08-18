678f4e823ead5dd0c7dd240a7803ba181d4a7282
brine <octgngamedirectory@gmail.com>
2020-08-17 23:39:02 -0400

    closes #2050 - a default shuffle command for piles optional 'shuffle' attribute for the group element, value is a hotkey string hotkey follows the same hierarchy patterns as group and card actions defaults to null, so it won't break existing shuffle python actions in games

--- octgnFX/Octgn.Library/Schemas/Game.cs
+++ octgnFX/Octgn.Library/Schemas/Game.cs
@@ -851,6 +851,8 @@ public partial class group {
     
     private string shortcutField;
     
+    private string shuffleField;
+    
     private boolean movetoField;
     
     private boolean collapsedField;
@@ -940,6 +942,17 @@ public partial class group {
     
     /// <remarks/>
     [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string shuffle {
+        get {
+            return this.shuffleField;
+        }
+        set {
+            this.shuffleField = value;
+        }
+    }
+    
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
     [System.ComponentModel.DefaultValueAttribute(boolean.True)]
     public boolean moveto {
         get {
@@ -1011,7 +1024,7 @@ public enum groupViewState {
 }
 
 /// <remarks/>
-[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
+[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.7.3081.0")]
 [System.SerializableAttribute()]
 [System.Diagnostics.DebuggerStepThroughAttribute()]
 [System.ComponentModel.DesignerCategoryAttribute("code")]
