6c91c410129813ba6fa3af152e250359369a66de
brine <octgngamedirectory@gmail.com>
2021-05-29 18:36:57 -0400

    fix a startup crash

--- octgnFX/Octgn.JodsEngine/Loaders/VersionedLoader.cs
+++ octgnFX/Octgn.JodsEngine/Loaders/VersionedLoader.cs
@@ -20,9 +20,9 @@ namespace Octgn.Loaders
             Versioned.Setup(Program.DeveloperMode);
             /* This section is automatically generated from the file Scripting/ApiVersions.xml. So, if you enjoy not getting pissed off, don't modify it.*/
             //START_REPLACE_API_VERSION
-			Versioned.RegisterVersion(Version.Parse("3.1.0.0"),DateTime.Parse("2014-1-12"),ReleaseMode.Live );
-			Versioned.RegisterVersion(Version.Parse("3.1.0.1"),DateTime.Parse("2014-1-22"),ReleaseMode.Live );
-			Versioned.RegisterVersion(Version.Parse("3.1.0.2"),DateTime.Parse("2015-8-26"),ReleaseMode.Live );
+			Versioned.RegisterVersion(Version.Parse("3.1.0.0"),DateTime.Parse("2014-1-12", System.Globalization.CultureInfo.InvariantCulture),ReleaseMode.Live );
+			Versioned.RegisterVersion(Version.Parse("3.1.0.1"),DateTime.Parse("2014-1-22", System.Globalization.CultureInfo.InvariantCulture),ReleaseMode.Live );
+			Versioned.RegisterVersion(Version.Parse("3.1.0.2"),DateTime.Parse("2015-8-26", System.Globalization.CultureInfo.InvariantCulture),ReleaseMode.Live );
 			Versioned.RegisterFile("PythonApi", "pack://application:,,,/Scripting/Versions/3.1.0.0.py", Version.Parse("3.1.0.0"));
 			Versioned.RegisterFile("PythonApi", "pack://application:,,,/Scripting/Versions/3.1.0.1.py", Version.Parse("3.1.0.1"));
 			Versioned.RegisterFile("PythonApi", "pack://application:,,,/Scripting/Versions/3.1.0.2.py", Version.Parse("3.1.0.2"));
