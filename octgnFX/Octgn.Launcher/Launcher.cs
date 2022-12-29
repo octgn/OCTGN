4227bffb73ca197974e5984455982191a969f0c9
Kelly Elton <its.the.doc@gmail.com>
2022-12-28 19:59:05 -0600

    convert to .net 6 with self contained.

---
+++ octgnFX/Octgn.Launcher/Launcher.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualBasic.Devices;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Octgn.Launcher;
+
+[SupportedOSPlatform("windows")]
+internal class Launcher
+{
+    private static log4net.ILog Log = log4net.LogManager.GetLogger("Launcher");
+
+    private static readonly Version Windows7Version = new Version(6, 1, 0, 0);
+
+    private const string DotNetDownloadUrl = "http://go.microsoft.com/fwlink/?LinkId=2085155";
+
+    private static bool IsDebugMode;
+
+    public Task RunAsync(CancellationToken cancellation) {
+        return Task.Run(() => Run(cancellation), cancellation);
+    }
+
+    public async Task Run(CancellationToken cancellation) {
+        Log.Info("Running Launcher");
+
+        cancellation.ThrowIfCancellationRequested();
+
+        {
+            var debugVariable = Environment.GetEnvironmentVariable("OCTGN_LAUNCHER_DEBUG");
+
+            IsDebugMode = debugVariable != null || Debugger.IsAttached;
+            Log.Info("IsDebugMode=" + IsDebugMode);
+
+            if (IsDebugMode && !Debugger.IsAttached) {
+                Log.Info("Launching Debugger");
+                var launched = Debugger.Launch();
+
+                if (launched) {
+                    Log.Info("Debugger attached");
+                } else {
+                    Log.Info("Debugger did not attach");
+                }
+            }
+        }
+
+        cancellation.ThrowIfCancellationRequested();
+
+        Version osVersion;
+        string osName;
+        {
+            var computerInfo = new ComputerInfo();
+
+            osVersion = Version.Parse(computerInfo.OSVersion);
+            osName = computerInfo.OSFullName;
+
+            Log.Info($"OS:{osName}:{osVersion}");
+        }
+
+        cancellation.ThrowIfCancellationRequested();
+
+        var workingDirectory = Environment.CurrentDirectory;
+        Log.Info("Working Dir=" + workingDirectory);
+
+        string commandLineArgs;
+        {
+            var exe = Environment.GetCommandLineArgs()[0];
+            var fullCommandLine = Environment.CommandLine;
+            commandLineArgs = fullCommandLine[exe.Length..];
+
+            Log.Info($"Args={commandLineArgs}");
+        }
+
+        cancellation.ThrowIfCancellationRequested();
+
+        var octgnPath = GetOctgnPath(IsDebugMode);
+        Log.Info($"Octgn Path={octgnPath}");
+
+        if (osVersion < Windows7Version) {
+            Log.Warn("Windows version is too low to run OCTGN.");
+
+            MessageBox.Show($"OCTGN is incompatible with {osName} {osVersion} at this time.{Environment.NewLine}{Environment.NewLine}OCTGN will now exit.", "Incompatible OS", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            return;
+        }
+
+        cancellation.ThrowIfCancellationRequested();
+
+        if (!IsDotNet48OrLaterInstalled()) {
+            Log.Warn(".net 4.8 is not installed.");
+
+            var result = MessageBox.Show($"OCTGN requires .net 4.8 to be installed.{Environment.NewLine}{Environment.NewLine}Press 'Yes' to download .net", ".Net Missing", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+
+            if (result == DialogResult.Yes) {
+                var download_path = await DownloadDotNetFramework48(cancellation);
+
+                if (download_path is not null) {
+                    Log.Info("Launching .net installer: " + download_path);
+
+                    // Launch process download_path, prompting for elevation
+                    try {
+                        using var process = Process.Start(new ProcessStartInfo(download_path) { Verb = "runas", UseShellExecute = true });
+
+                        if (process is null) throw new InvalidOperationException("Process.Start returned null");
+
+                        await process.WaitForExitAsync(cancellation);
+
+                        if (process.ExitCode != 0) {
+                            Log.Error("Running .net installer failed with exit code " + process.ExitCode);
+
+                            return;
+                        }
+                    } catch (Win32Exception ex) when (ex.NativeErrorCode == 1223) {
+                        Log.Error("User cancelled .net installer by declining elevation prompt");
+
+                        return;
+                    }
+                } else {
+                    Log.Warn("Failed to download .net installer");
+
+                    return;
+                }
+            } else {
+                Log.Warn("User cancelled .net download");
+
+                return;
+            }
+        }
+
+        cancellation.ThrowIfCancellationRequested();
+
+        if (!LaunchOctgn(octgnPath, workingDirectory, commandLineArgs)) {
+            Log.Warn("Could not launch OCTGN");
+
+            MessageBox.Show($"OCTGN was unable to be launched. Please try again later", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return;
+        }
+
+        Log.Info("Launcher ran successfully");
+    }
+
+    private static async Task<string?> DownloadDotNetFramework48(CancellationToken cancellation) {
+        Log.Info("Creating download path");
+
+        var downloadPath = Path.Combine(Path.GetTempPath(), "netinstaller" + DateTime.Now.Ticks + ".exe");
+
+        Log.Info($"Downloading .net installer from {DotNetDownloadUrl} to {downloadPath}");
+
+        try {
+#pragma warning disable SYSLIB0014 // Type or member is obsolete
+            using (var wc = new WebClient()) {
+                await wc.DownloadFileTaskAsync(DotNetDownloadUrl, downloadPath).WaitAsync(cancellation);
+
+                if (cancellation.IsCancellationRequested) {
+                    Log.Warn("Download cancelled");
+
+                    wc.CancelAsync();
+
+                    return null;
+                }
+            }
+#pragma warning restore SYSLIB0014 // Type or member is obsolete
+
+            return downloadPath;
+        } catch (OperationCanceledException) {
+            Log.Error("Download cancelled");
+
+            return null;
+        } catch (Exception ex) {
+            Log.Error("Error downloading installer " + DotNetDownloadUrl, ex);
+
+            MessageBox.Show($"There was an error downloading the .Net Framework", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return null;
+        }
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static bool IsDotNet48OrLaterInstalled() {
+        Log.Info("Getting .net from registry");
+
+        try {
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\")) {
+                if (ndpKey == null) {
+                    Log.Warn("No .net registry key found.");
+
+                    return false;
+                }
+
+                var releaseKey = Convert.ToInt32(ndpKey.GetValue("Release"));
+
+                Log.Info(".Net Release: " + releaseKey);
+
+                if (releaseKey >= 528040) {
+                    return true;
+                }
+
+                return false;
+            }
+        } catch (Exception ex) {
+            Log.Error("Error getting .net from registry", ex);
+
+            return false;
+        }
+    }
+
+    private static string GetOctgnPath(bool isDebug) {
+        var root_dir = System.AppContext.BaseDirectory;
+
+        if (root_dir is null)
+            throw new InvalidOperationException("Could not get app directory");
+
+        var octgnPath = Path.Combine(root_dir, "Octgn.exe");
+
+        if (isDebug) {
+            octgnPath = "..\\..\\..\\..\\..\\Octgn\\bin\\Debug\\Octgn.exe";
+        }
+
+        return octgnPath;
+    }
+
+    private static bool LaunchOctgn(string octgnPath, string workingDirectory, string commandLineArgs) {
+        try {
+            var psi = new ProcessStartInfo(octgnPath) {
+                Arguments = commandLineArgs,
+                WorkingDirectory = workingDirectory
+            };
+
+            Process.Start(psi);
+
+            return true;
+        } catch (Exception ex) {
+            Log.Error("Error launching octgn", ex);
+
+            return false;
+        }
+    }
+}
---
+++ octgnFX/Octgn.Launcher/Main.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Octgn.Launcher
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Verifying system requirements and launching OCTGN...";
+            this.label1.UseWaitCursor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 41);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(391, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 84);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launching OCTGN";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
