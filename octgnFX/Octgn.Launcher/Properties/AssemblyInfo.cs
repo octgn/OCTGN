5c6b2c574e497200e8bd1fe6e8c5c6d98f107e80
Kelly Elton <its.the.doc@gmail.com>
2022-12-28 19:59:05 -0600

    convert to .net 6 with self contained.

--- octgnFX/Octgn.Launcher/Properties/AssemblyInfo.cs
+++ octgnFX/Octgn.Launcher/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -29,15 +28,5 @@ using System.Windows;
 //the line below to match the UICulture setting in the project file.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
-)]
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
