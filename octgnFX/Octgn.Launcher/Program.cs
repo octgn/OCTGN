8c6c153636705ab1e66989e6091b4a21f4e8c60b
Kelly Elton <its.the.doc@gmail.com>
2022-12-28 20:44:23 -0600

    Back to .net 4.5, but winformz

--- octgnFX/Octgn.Launcher/Program.cs
+++ octgnFX/Octgn.Launcher/Program.cs
@@ -1,54 +1,53 @@
 ﻿using System;
-using System.Runtime.Versioning;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace Octgn.Launcher;
-
-[SupportedOSPlatform("windows")]
-class Program : ApplicationContext
+namespace Octgn.Launcher
 {
-    private static log4net.ILog Log = log4net.LogManager.GetLogger("Program");
+    class Program : ApplicationContext
+    {
+        private static log4net.ILog Log = log4net.LogManager.GetLogger("Program");
 
-    [STAThread]
-    static void Main() {
-        Log.Info("Starting");
+        [STAThread]
+        static void Main() {
+            Log.Info("Starting");
 
-        CancellationTokenSource? cts = null;
-        try {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            CancellationTokenSource cts = null;
+            try {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            var launcher = new Launcher();
-            cts = new CancellationTokenSource();
+                var launcher = new Launcher();
+                cts = new CancellationTokenSource();
 
-            Main window = new(launcher, cts.Token) {
-                Opacity = 0,
-                ShowInTaskbar = false
-            };
+                var window = new Main(launcher, cts.Token) {
+                    Opacity = 0,
+                    ShowInTaskbar = false
+                };
 
-            Application.Run(window);
-        } catch (Exception ex) {
-            Log.Error("Error running launcher", ex);
+                Application.Run(window);
+            } catch (Exception ex) {
+                Log.Error("Error running launcher", ex);
 
-            Environment.ExitCode = 1;
+                Environment.ExitCode = 1;
 
-            // Show message to user that error occured
-            MessageBox.Show("An error occured while running the launcher. Please check the log files for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
+                // Show message to user that error occured
+                MessageBox.Show("An error occured while running the launcher. Please check the log files for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
-        try {
-            cts?.Cancel();
-        } catch (ObjectDisposedException) {
-        } catch (Exception ex) {
-            Log.Error("Error cancelling cancellation token source", ex);
-        }
+            try {
+                cts?.Cancel();
+            } catch (ObjectDisposedException) {
+            } catch (Exception ex) {
+                Log.Error("Error cancelling cancellation token source", ex);
+            }
 
-        try {
-            cts?.Dispose();
-        } catch (ObjectDisposedException) {
-        } catch (Exception ex) {
-            Log.Error("Error disposing cancellation token source", ex);
+            try {
+                cts?.Dispose();
+            } catch (ObjectDisposedException) {
+            } catch (Exception ex) {
+                Log.Error("Error disposing cancellation token source", ex);
+            }
         }
     }
 }
