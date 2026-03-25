49ac2178623c3052dad8cea46732fc9db8b967dd
root <root@claw1.homer.lan>
2026-03-25 14:57:41 -0500

    Fix #1915: check reserved property names in o8build

--- octgnFX/o8build/GameValidator.cs
+++ octgnFX/o8build/GameValidator.cs
@@ -467,9 +467,19 @@
 
             if (game.card.property != null && game.card.property.Any())
             {
+                var reservedNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+                    "name", "id", "alternate", "size", "type", "set"
+                };
+
                 List<string> props = new List<string>();
                 foreach (var prop in game.card.property)
                 {
+                    if (reservedNames.Contains(prop.name))
+                    {
+                        throw new UserMessageException("Property '{0}' is a reserved name and cannot be used in file: {1}", prop.name, Path.Combine(Directory.FullName, "defintion.xml"));
+                    }
+
                     if (!props.Contains(prop.name))
                     {
                         props.Add(prop.name);
